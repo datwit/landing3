a87f37bd858d752a658a7acc4744dfb596cdc723
Enma Lidia Munoz Garcia <enmalidia88@gmail.com>
2021-02-27 15:43:59 -0500

    style: :lipstick: Benefits section layout: adjusting responsive
    #24

--- components/partial/Iconblock.vue
+++ components/partial/Iconblock.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <div class="h-full overflow-hidden mx-4 md:mx-0">
-      <img class="lg:h-48 md:h-36 w-full object-cover object-center p-8" :src="img" :alt="alt">
-      <div class="p-4 text-center">
-        <h1 class="title-font text-lg font-bold text-primary mb-3">{{title}}</h1>
-        <p class="mt-2 text-base leading-6 text-gray-500">{{descrip}}</p>
+      <img class="mx-auto h-9/12 w-9/12 sm:w-full sm:h-full object-cover object-center px-8" :src="img" :alt="alt">
+      <div class="md:p-4 text-center">
+        <h1 class="title-font text-sm sm:text-lg font-bold text-primary md:mb-3">{{title}}</h1>
+        <p class="text-sm sm:text-base leading-5 text-gray-500 mb-3">{{descrip}}</p>
       </div>
     </div>
   </div>
--- components/team/Benefits.vue
+++ components/team/Benefits.vue
@@ -2,10 +2,10 @@
   <section class="bg-white mx-auto max-w-screen-xl px-10">
     <cheader></cheader>
     <div class="container px-5 mx-auto">
-      <h2 class="mt-4">We take care of our people</h2>
+      <h2 class="mt-0">We take care of our people</h2>
       <h4 class="mb-6">Lorem, ipsum dolor sit amet consectetur adipisicing elit...</h4>
         <div class="flex flex-wrap -m-4">
-          <div class="p-4 md:w-1/3">
+          <div class="md:p-4 sm:w-1/3">
             <Iconblock
               img = "https://dummyimage.com/720x400"
               alt = "Img"
@@ -13,7 +13,7 @@
               descrip = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam consequuntur facilis"
             />
           </div>
-          <div class="p-4 md:w-1/3">
+          <div class="md:p-4 sm:w-1/3">
             <Iconblock
               img = "https://dummyimage.com/720x400"
               alt = "Img"
@@ -21,7 +21,7 @@
               descrip = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam consequuntur facilis"
             />
           </div>
-          <div class="p-4 md:w-1/3">
+          <div class="md:p-4 sm:w-1/3">
             <Iconblock
               img = "https://dummyimage.com/720x400"
               alt = "Img"
