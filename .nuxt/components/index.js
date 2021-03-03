eb83c40f8ae8306bbaa0463ad9b32c41387b7f9d
Enma Lidia Munoz Garcia <enmalidia88@gmail.com>
2021-03-03 14:10:53 -0500

    refactor: :recycle: Redoing and refactoring the collpasible code
    #27

--- .nuxt/components/index.js
+++ .nuxt/components/index.js
@@ -8,14 +8,15 @@ export { default as Mainpricing } from '../..\\components\\home\\Mainpricing.vue
 export { default as Scases } from '../..\\components\\home\\Scases.vue'
 export { default as Selection } from '../..\\components\\home\\Selection.vue'
 export { default as Services } from '../..\\components\\home\\Services.vue'
-export { default as Iconblock } from '../..\\components\\partial\\Iconblock.vue'
-export { default as Scrollsign } from '../..\\components\\partial\\Scrollsign.vue'
-export { default as Studycase } from '../..\\components\\partial\\Studycase.vue'
 export { default as Faqs } from '../..\\components\\partner\\Faqs.vue'
 export { default as Finding } from '../..\\components\\partner\\Finding.vue'
 export { default as Partnerchoosing } from '../..\\components\\partner\\Partnerchoosing.vue'
 export { default as Benefits } from '../..\\components\\team\\Benefits.vue'
 export { default as Tech } from '../..\\components\\team\\Tech.vue'
+export { default as Collapsible } from '../..\\components\\partial\\Collapsible.vue'
+export { default as Iconblock } from '../..\\components\\partial\\Iconblock.vue'
+export { default as Scrollsign } from '../..\\components\\partial\\Scrollsign.vue'
+export { default as Studycase } from '../..\\components\\partial\\Studycase.vue'
 
 export const LazyAuthor = import('../..\\components\\global\\Author.vue' /* webpackChunkName: "components_global/Author" */).then(c => c.default || c)
 export const LazyCta = import('../..\\components\\global\\CTA.vue' /* webpackChunkName: "components_global/CTA" */).then(c => c.default || c)
@@ -27,11 +28,12 @@ export const LazyMainpricing = import('../..\\components\\home\\Mainpricing.vue'
 export const LazyScases = import('../..\\components\\home\\Scases.vue' /* webpackChunkName: "components_home/Scases" */).then(c => c.default || c)
 export const LazySelection = import('../..\\components\\home\\Selection.vue' /* webpackChunkName: "components_home/Selection" */).then(c => c.default || c)
 export const LazyServices = import('../..\\components\\home\\Services.vue' /* webpackChunkName: "components_home/Services" */).then(c => c.default || c)
-export const LazyIconblock = import('../..\\components\\partial\\Iconblock.vue' /* webpackChunkName: "components_partial/Iconblock" */).then(c => c.default || c)
-export const LazyScrollsign = import('../..\\components\\partial\\Scrollsign.vue' /* webpackChunkName: "components_partial/Scrollsign" */).then(c => c.default || c)
-export const LazyStudycase = import('../..\\components\\partial\\Studycase.vue' /* webpackChunkName: "components_partial/Studycase" */).then(c => c.default || c)
 export const LazyFaqs = import('../..\\components\\partner\\Faqs.vue' /* webpackChunkName: "components_partner/Faqs" */).then(c => c.default || c)
 export const LazyFinding = import('../..\\components\\partner\\Finding.vue' /* webpackChunkName: "components_partner/Finding" */).then(c => c.default || c)
 export const LazyPartnerchoosing = import('../..\\components\\partner\\Partnerchoosing.vue' /* webpackChunkName: "components_partner/Partnerchoosing" */).then(c => c.default || c)
 export const LazyBenefits = import('../..\\components\\team\\Benefits.vue' /* webpackChunkName: "components_team/Benefits" */).then(c => c.default || c)
 export const LazyTech = import('../..\\components\\team\\Tech.vue' /* webpackChunkName: "components_team/Tech" */).then(c => c.default || c)
+export const LazyCollapsible = import('../..\\components\\partial\\Collapsible.vue' /* webpackChunkName: "components_partial/Collapsible" */).then(c => c.default || c)
+export const LazyIconblock = import('../..\\components\\partial\\Iconblock.vue' /* webpackChunkName: "components_partial/Iconblock" */).then(c => c.default || c)
+export const LazyScrollsign = import('../..\\components\\partial\\Scrollsign.vue' /* webpackChunkName: "components_partial/Scrollsign" */).then(c => c.default || c)
+export const LazyStudycase = import('../..\\components\\partial\\Studycase.vue' /* webpackChunkName: "components_partial/Studycase" */).then(c => c.default || c)
