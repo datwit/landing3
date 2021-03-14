e716958868caa5e1e55510168d0b40d96227bd1c
Enma Lidia Muñoz Garcia <enmalidia88@gmail.com>
2021-03-14 17:32:24 -0400

    feat: :sparkles: Benefits section: Setting overall layout
    Missing styles and images
    
    #37

--- src/components/Benefits/index.js
+++ src/components/Benefits/index.js
@@ -2,9 +2,45 @@ import React from 'react'
 
 const Benefits = () => {
     return (
-        <div>
-            
-        </div>
+        <div class="container px-5 mx-auto">
+        <h2 class="mt-0">We take care of our people</h2>
+        <h4 class="mb-6">Lorem, ipsum dolor sit amet consectetur adipisicing elit...</h4>
+            <div class="flex flex-wrap -m-4">
+                <div class="md:p-4 sm:w-1/3">
+                    <div>
+                        <div class="h-full overflow-hidden mx-4 md:mx-0">
+                        <img class="mx-auto h-9/12 w-9/12 sm:w-full sm:h-full object-cover object-center px-8" src="" alt="image"/>
+                        <div class="md:p-4 text-center">
+                            <h1 class="title-font text-sm sm:text-lg font-bold text-primary md:mb-3">Lorem ipsum dolor si amet</h1>
+                            <p class="text-sm sm:text-base leading-5 text-gray-500 mb-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam consequuntur facilis</p>
+                        </div>
+                        </div>
+                    </div>              
+                </div>
+                <div class="md:p-4 sm:w-1/3">
+                    <div>
+                        <div class="h-full overflow-hidden mx-4 md:mx-0">
+                        <img class="mx-auto h-9/12 w-9/12 sm:w-full sm:h-full object-cover object-center px-8" src="" alt="image"/>
+                        <div class="md:p-4 text-center">
+                            <h1 class="title-font text-sm sm:text-lg font-bold text-primary md:mb-3">Lorem ipsum dolor si amet</h1>
+                            <p class="text-sm sm:text-base leading-5 text-gray-500 mb-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam consequuntur facilis</p>
+                        </div>
+                        </div>
+                    </div>              
+                </div>
+                <div class="md:p-4 sm:w-1/3">
+                    <div>
+                        <div class="h-full overflow-hidden mx-4 md:mx-0">
+                        <img class="mx-auto h-9/12 w-9/12 sm:w-full sm:h-full object-cover object-center px-8" src="" alt="image"/>
+                        <div class="md:p-4 text-center">
+                            <h1 class="title-font text-sm sm:text-lg font-bold text-primary md:mb-3">Lorem ipsum dolor si amet</h1>
+                            <p class="text-sm sm:text-base leading-5 text-gray-500 mb-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam consequuntur facilis</p>
+                        </div>
+                        </div>
+                    </div>              
+                </div>            
+            </div>
+      </div>
     )
 }
 
