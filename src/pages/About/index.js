9bf76f1ba5d3aa8b8ce425591b6e2fafffee8ed2
Enma Lidia Muñoz Garcia <enmalidia88@gmail.com>
2021-03-14 19:18:36 -0400

    feat: :sparkles: About Datwit section: Setting basic layout
    Missing styling and illustration
    
    #40

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -2,14 +2,22 @@ import React from 'react'
 import CustomControls from '../../components/Slide/CustomControls'
 import { FullPage, Slide } from 'react-full-page'
 import Section from '../../components/Section'
+import Footer from '../../components/Footer'
+import Datwit from '../../components/AboutUs/Datwit'
 
 const About = () => {
     return (
         <FullPage controls={CustomControls}>
         <Slide>
           <Section>
-           
+            <Datwit />
           </Section>        
+        </Slide>
+
+        <Slide>
+          <section className= "w-full h-screen bg-primary mx-auto px-10">
+            <Footer />
+          </section>                  
         </Slide>
         </FullPage>
 
