48df95d58a998181ffc64bf816c46c75843a030a
Enma Lidia Munoz Garcia <enmalidia88@gmail.com>
2021-03-15 15:09:22 -0400

    style: :art: Redoing contact form layout and adjusting it to our content
    #41

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,96 +1,64 @@
 import React from 'react'
 
-const ContactForm = () => {
+const ContacthtmlForm = () => {
     return (
-        <div className="container mx-auto flex sm:flex-nowrap flex-wrap">
-            <div className="w-full lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
-                <iframe
-                width="100%"
-                height="100%"
-                className="absolute inset-0"
-                frameborder="0"
-                title="map"
-                marginheight="0"
-                marginwidth="0"
-                scrolling="no"
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2829.912127445988!2d20.40630131515939!3d44.82335478399543!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x475a6579cfc71f57%3A0xb1db7bd4fc85870e!2sBulevar%20Zorana%20%C4%90in%C4%91i%C4%87a%20116%2C%20Beograd%2011070!5e0!3m2!1sen!2srs!4v1609605711401!5m2!1sen!2srs"
-                
-                ></iframe>
-                <div className="bg-white relative flex flex-wrap py-6 rounded shadow-md">
-                    <div className="lg:w-1/2 px-6">
-                        <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">
-                        ADDRESS
-                        </h2>
-                        <p className="mt-1">
-                        Bulevar Zorana Đinđića 116, floor 7, apto 47, Belgrade 11070
-                        </p>
+        <div className="container px-5 mx-auto">
+            <h2 className="mt-4">Check out our solutions</h2>
+            <h4>Commitment and creativity mark our creations. By all means, stroll along...</h4>
+            <div className="flex flex-wrap -m-4 h-2/3 mt-6">
+                <div className="w-full md:w-1/2 lg:w-2/3 relative">
+                    <iframe
+                        width="100%"
+                        height="100%"
+                        className="absolute inset-0"
+                        frameBorder="0"
+                        title="map"
+                        marginHeight="0"
+                        marginWidth="0"
+                        scrolling="no"
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2829.912127445988!2d20.40630131515939!3d44.82335478399543!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x475a6579cfc71f57%3A0xb1db7bd4fc85870e!2sBulevar%20Zorana%20%C4%90in%C4%91i%C4%87a%20116%2C%20Beograd%2011070!5e0!3m2!1sen!2srs!4v1609605711401!5m2!1sen!2srs">
+                    </iframe>
+                </div>
+                <div className="w-full md:w-1/2 lg:w-1/3 pl-10">
+                    <div className="bg-white w-full md:mt-0">                        
+                        <p className="leading-relaxed m-0 text-gray-600">Our doors are always open, so feel free to drop by and spot us in our natural habitat</p>
+                        <div className="relative mb-2">
+                            <label htmlFor="name" className="leading-7 text-sm text-gray-600">Name</label>
+                            <input
+                                id="name"
+                                type="text"
+                                name="name"
+                                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                            />
+                        </div>
+                        <div className="relative mb-2">
+                            <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email</label>
+                            <input
+                                id="email"
+                                type="email"
+                                name="email"
+                                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                            />
+                        </div>
+                        <div className="relative mb-2">
+                            <label htmlFor="message" className="leading-7 text-sm text-gray-600">Message</label>
+                            <textarea
+                                id="message"
+                                name="message"
+                                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                            ></textarea>
+                        </div>
+                        <div className="justify-center select-none flex">
+                            <a className="btn inline-flex" href="/contact">Send
+                            <svg className="h-6 w-6 text-white ml-2"  width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <line x1="10" y1="14" x2="21" y2="3" />  <path d="M21 3L14.5 21a.55 .55 0 0 1 -1 0L10 14L3 10.5a.55 .55 0 0 1 0 -1L21 3" /></svg>
+                            </a>
+                        </div>
+                        <p className="text-xs text-gray-500 mt-3">Chicharrones blog helvetica normcore iceland tousled brook viral artisan.</p>
                     </div>
-                <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
-                    <h2
-                    className="title-font font-semibold text-gray-900 tracking-widest text-xs"
-                    >
-                    EMAIL
-                    </h2>
-                    <a className="text-indigo-500 leading-relaxed">example@email.com</a>
-                    <h2
-                    className="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4"
-                    >
-                    PHONE
-                    </h2>
-                    <p className="leading-relaxed">123-456-7890</p>
-                </div>
-                </div>
-            </div>
-            <div className="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-                <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">
-                Feedback
-                </h2>
-                <p className="leading-relaxed mb-5 text-gray-600">
-                Post-ironic portland shabby chic echo park, banjo fashion axe
-                </p>
-                <div className="relative mb-4">
-                <label for="name" className="leading-7 text-sm text-gray-600">Name</label>
-                <input
-                    id="name"
-                    type="text"
-                    name="name"
-                    className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                />
-                </div>
-                <div className="relative mb-4">
-                <label for="email" className="leading-7 text-sm text-gray-600"
-                    >Email</label
-                >
-                <input
-                    id="email"
-                    type="email"
-                    name="email"
-                    className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                />
-                </div>
-                <div className="relative mb-4">
-                <label for="message" className="leading-7 text-sm text-gray-600"
-                    >Message</label
-                >
-                <textarea
-                    id="message"
-                    name="message"
-                    className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-                ></textarea>
-                </div>
-                <button
-                className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-                aria-label="Send"
-                >
-                Send
-                </button>
-                <p className="text-xs text-gray-500 mt-3">
-                Chicharrones blog helvetica normcore iceland tousled brook viral
-                artisan.
-                </p>
-            </div>
+                </div>                
+            </div>            
         </div>
     )
 }
 
-export default ContactForm
+export default ContacthtmlForm
