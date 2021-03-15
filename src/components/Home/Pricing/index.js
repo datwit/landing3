dce4ddb8bd1a02a5e4c963e3bf92891bdfdf3d9c
Enma Lidia Munoz Garcia <enmalidia88@gmail.com>
2021-03-15 11:33:41 -0400

    feat: :lipstick: Pricing section: Adding responsive carousel
    Adjusting styles
    
    #36

--- src/components/Home/Pricing/index.js
+++ src/components/Home/Pricing/index.js
@@ -1,7 +1,33 @@
 import React from 'react'
 import * as DATA from "./data"
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 const index = () => {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,        
+        responsive: [
+
+        {
+            breakpoint: 768,
+            settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            initialSlide: 0
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+            }
+        }
+        ]
+    }
     return (
         
             <div className="container px-5 mx-auto">
@@ -36,7 +62,8 @@ const index = () => {
                         </div>
                         {/*--------------------------------------------------------------------------------*/}
                         <div className="p-4 md:w-1/3 w-full">
-                            <div className="md:h-full p-3 border-2 border-gray-200 border-opacity-60 overflow-hidden">
+                            <div className="md:h-full p-3 border-2 border-secondary2 border-opacity-60 overflow-hidden relative">
+                                <span className="bg-secondary2 text-white px-3 py-1 tracking-widest text-xs absolute right-0 top-0 rounded-bl">POPULAR</span>
                                 <h2 className="text-sm tracking-widest title-font mb-1 font-medium pt-2 h-12">{DATA.CARD2.field}</h2>
                                 <h1 className="text-5xl text-primary mb-1 leading-none mx-auto">{DATA.CARD2.price}</h1>
                                 <p className="text-xs text-gray-500 mt-3 border-b border-gray-200">{DATA.CARD2.description}</p>
@@ -87,6 +114,83 @@ const index = () => {
                         <a className="btn" href="/contact">Contact us</a>
                         </div>
                     </div>
+                </div>
+                <div className="md:hidden">
+                    <Slider {...settings}> 
+                        <div>
+                            <div className="p-4 md:w-1/3 w-full">
+                                <div className="md:h-full p-3 border-2 border-gray-200 border-opacity-60 overflow-hidden">
+                                    <h2 className="text-sm tracking-widest title-font mb-1 font-medium pt-2 h-12">{DATA.CARD1.field}</h2>
+                                    <h1 className="text-5xl text-primary mb-1 leading-none mx-auto">{DATA.CARD1.price}</h1>
+                                    <p className="text-xs text-gray-500 mt-3 border-b border-gray-200">{DATA.CARD1.description}</p>
+                                    <p className="flex items-center text-gray-600 mb-2 text-xs sm:text-sm">
+                                        <span className="w-5 h-5 mr-2 inline-flex items-center justify-center flex-shrink-0">
+                                        <svg className="h-5 w-5 text-secondary2"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="4"  strokeLinecap="round"  strokeLinejoin="round">  <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14" />  <polyline points="22 4 12 14.01 9 11.01" /></svg>
+                                        </span>{DATA.CARD1.b1}
+                                    </p>
+                                    <p className="flex items-center text-gray-600 mb-2 text-xs sm:text-sm">
+                                        <span className="w-5 h-5 mr-2 inline-flex items-center justify-center flex-shrink-0">
+                                        <svg className="h-5 w-5 text-secondary2"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="4"  strokeLinecap="round"  strokeLinejoin="round">  <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14" />  <polyline points="22 4 12 14.01 9 11.01" /></svg>
+                                        </span>{DATA.CARD1.b2}
+                                    </p>
+                                    <p className="flex items-center text-gray-600 mb-2 text-xs sm:text-sm">
+                                        <span className="w-5 h-5 mr-2 inline-flex items-center justify-center flex-shrink-0">
+                                        <svg className="h-5 w-5 text-secondary2"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="4"  strokeLinecap="round"  strokeLinejoin="round">  <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14" />  <polyline points="22 4 12 14.01 9 11.01" /></svg>
+                                        </span>{DATA.CARD1.b3}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                       <div>
+                            <div className="p-4 md:w-1/3 w-full">
+                                <div className="md:h-full p-3 border-2 border-secondary2 border-opacity-60 overflow-hidden relative">
+                                    <span className="bg-secondary2 text-white px-3 py-1 tracking-widest text-xs absolute right-0 top-0 rounded-bl">POPULAR</span>
+                                    <h2 className="text-sm tracking-widest title-font mb-1 font-medium pt-2 h-12">{DATA.CARD2.field}</h2>
+                                    <h1 className="text-5xl text-primary mb-1 leading-none mx-auto">{DATA.CARD2.price}</h1>
+                                    <p className="text-xs text-gray-500 mt-3 border-b border-gray-200">{DATA.CARD2.description}</p>
+                                    <p className="flex items-center text-gray-600 mb-2 text-xs sm:text-sm">
+                                        <span className="w-5 h-5 mr-2 inline-flex items-center justify-center flex-shrink-0">
+                                        <svg className="h-5 w-5 text-secondary2"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="4"  strokeLinecap="round"  strokeLinejoin="round">  <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14" />  <polyline points="22 4 12 14.01 9 11.01" /></svg>
+                                        </span>{DATA.CARD2.b1}
+                                    </p>
+                                    <p className="flex items-center text-gray-600 mb-2 text-xs sm:text-sm">
+                                        <span className="w-5 h-5 mr-2 inline-flex items-center justify-center flex-shrink-0">
+                                        <svg className="h-5 w-5 text-secondary2"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="4"  strokeLinecap="round"  strokeLinejoin="round">  <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14" />  <polyline points="22 4 12 14.01 9 11.01" /></svg>
+                                        </span>{DATA.CARD2.b2}
+                                    </p>
+                                    <p className="flex items-center text-gray-600 mb-2 text-xs sm:text-sm">
+                                        <span className="w-5 h-5 mr-2 inline-flex items-center justify-center flex-shrink-0">
+                                        <svg className="h-5 w-5 text-secondary2"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="4"  strokeLinecap="round"  strokeLinejoin="round">  <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14" />  <polyline points="22 4 12 14.01 9 11.01" /></svg>
+                                        </span>{DATA.CARD2.b3}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                       <div>
+                            <div className="p-4 md:w-1/3 w-full">
+                                <div className="md:h-full p-3 border-2 border-gray-200 border-opacity-60 overflow-hidden">
+                                    <h2 className="text-sm tracking-widest title-font mb-1 font-medium pt-2 h-12">{DATA.CARD2.field}</h2>
+                                    <h1 className="text-5xl text-primary mb-1 leading-none mx-auto">{DATA.CARD2.price}</h1>
+                                    <p className="text-xs text-gray-500 mt-3 border-b border-gray-200">{DATA.CARD2.description}</p>
+                                    <p className="flex items-center text-gray-600 mb-2 text-xs sm:text-sm">
+                                        <span className="w-5 h-5 mr-2 inline-flex items-center justify-center flex-shrink-0">
+                                        <svg className="h-5 w-5 text-secondary2"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="4"  strokeLinecap="round"  strokeLinejoin="round">  <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14" />  <polyline points="22 4 12 14.01 9 11.01" /></svg>
+                                        </span>{DATA.CARD2.b1}
+                                    </p>
+                                    <p className="flex items-center text-gray-600 mb-2 text-xs sm:text-sm">
+                                        <span className="w-5 h-5 mr-2 inline-flex items-center justify-center flex-shrink-0">
+                                        <svg className="h-5 w-5 text-secondary2"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="4"  strokeLinecap="round"  strokeLinejoin="round">  <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14" />  <polyline points="22 4 12 14.01 9 11.01" /></svg>
+                                        </span>{DATA.CARD2.b2}
+                                    </p>
+                                    <p className="flex items-center text-gray-600 mb-2 text-xs sm:text-sm">
+                                        <span className="w-5 h-5 mr-2 inline-flex items-center justify-center flex-shrink-0">
+                                        <svg className="h-5 w-5 text-secondary2"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="4"  strokeLinecap="round"  strokeLinejoin="round">  <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14" />  <polyline points="22 4 12 14.01 9 11.01" /></svg>
+                                        </span>{DATA.CARD2.b3}
+                                    </p>
+                                </div>
+                            </div>
+                       </div>
+                    </Slider>
                 </div>
             </div>
         
