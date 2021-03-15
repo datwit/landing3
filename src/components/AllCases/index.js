736bd56a8a1d4d3f4eeeb8db885872348b11c9cb
Enma Lidia Munoz Garcia <enmalidia88@gmail.com>
2021-03-15 13:36:37 -0400

    refactor: :recycle: Refactoring code: adding data source and adjust code to explore data
    #39

--- src/components/AllCases/index.js
+++ src/components/AllCases/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import data from "./data.json"
 
 const AllCases = () => {
     return (
@@ -6,61 +7,29 @@ const AllCases = () => {
             <h2 className="mt-4">Check out our solutions</h2>
             <h4 className="mb-6">Commitment and creativity mark our creations. By all means, stroll along...</h4>
             <div className="hidden md:block">
-                <div className="flex flex-wrap -m-4">          
-                <div className="p-4 md:w-1/3">
-                        <div className="h-full border-2 border-gray-200 border-opacity-60 overflow-hidden mx-4 md:mx-0">
-                            <img className="lg:h-48 md:h-28 w-full object-cover object-center" src="" alt="img" />
-                            <div className="p-4">
-                                {/* <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2> */}                
-                                <h1 className="title-font text-lg font-bold text-primary mb-1">Anim magna aliqua cillum sint minim </h1>                
-                                <p className="mt-0 text-base leading-6 text-gray-500">Minim nisi aliquip tempor consequat incididunt nulla veniam officia dolore elit labore laboris.</p>
-                                <div className="flex items-center flex-wrap ">
-                                <a className="text-secondary2 inline-flex items-center md:mb-2 lg:mb-0">Learn More
-                                    <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                    <path d="M5 12h14"></path>
-                                    <path d="M12 5l7 7-7 7"></path>
-                                    </svg>
-                                </a>
-                                </div> 
+                <div className="flex flex-wrap -m-4">
+                    {
+                        data.map((d, key) => (
+                            <div className="p-4 md:w-1/3" key={key}>
+                                <div className="h-full border-2 border-gray-200 border-opacity-60 overflow-hidden mx-4 md:mx-0">
+                                    <img className="lg:h-48 md:h-28 w-full object-cover object-center" src={d.src} alt={d.alt} />
+                                    <div className="p-4">
+                                        {/* <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2> */}
+                                        <h1 className="title-font text-lg font-bold text-primary mb-1">{d.title}</h1>
+                                        <p className="mt-0 text-base leading-6 text-gray-500">{d.description}</p>
+                                        <div className="flex items-center flex-wrap ">
+                                            <a className="text-secondary2 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+                                                <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                                    <path d="M5 12h14"></path>
+                                                    <path d="M12 5l7 7-7 7"></path>
+                                                </svg>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div> 
-                    <div className="p-4 md:w-1/3">
-                        <div className="h-full border-2 border-gray-200 border-opacity-60 overflow-hidden mx-4 md:mx-0">
-                            <img className="lg:h-48 md:h-28 w-full object-cover object-center" src="" alt="img" />
-                            <div className="p-4">
-                                {/* <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2> */}                
-                                <h1 className="title-font text-lg font-bold text-primary mb-1">Anim magna aliqua cillum sint minim </h1>                
-                                <p className="mt-0 text-base leading-6 text-gray-500">Minim nisi aliquip tempor consequat incididunt nulla veniam officia dolore elit labore laboris.</p>
-                                <div className="flex items-center flex-wrap ">
-                                <a className="text-secondary2 inline-flex items-center md:mb-2 lg:mb-0">Learn More
-                                    <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                    <path d="M5 12h14"></path>
-                                    <path d="M12 5l7 7-7 7"></path>
-                                    </svg>
-                                </a>
-                                </div> 
-                            </div>
-                        </div>
-                    </div> 
-                    <div className="p-4 md:w-1/3">
-                        <div className="h-full border-2 border-gray-200 border-opacity-60 overflow-hidden mx-4 md:mx-0">
-                            <img className="lg:h-48 md:h-28 w-full object-cover object-center" src="" alt="img" />
-                            <div className="p-4">
-                                {/* <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2> */}                
-                                <h1 className="title-font text-lg font-bold text-primary mb-1">Anim magna aliqua cillum sint minim </h1>                
-                                <p className="mt-0 text-base leading-6 text-gray-500">Minim nisi aliquip tempor consequat incididunt nulla veniam officia dolore elit labore laboris.</p>
-                                <div className="flex items-center flex-wrap ">
-                                <a className="text-secondary2 inline-flex items-center md:mb-2 lg:mb-0">Learn More
-                                    <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                    <path d="M5 12h14"></path>
-                                    <path d="M12 5l7 7-7 7"></path>
-                                    </svg>
-                                </a>
-                                </div> 
-                            </div>
-                        </div>
-                    </div>                
+                        ))
+                    }
                 </div>
             </div>
         </div>
