87c38668829d42723a3b0271519073b4a02b9d0d
Enma Lidia Munoz Garcia <enmalidia88@gmail.com>
2021-03-15 11:13:38 -0400

    feat: :sparkles: Updating study cases section: Adding responsive carousel
    #36

--- src/components/Home/StudyCases/index.js
+++ src/components/Home/StudyCases/index.js
@@ -1,8 +1,35 @@
 import React from 'react'
 import * as DATA from "./data"
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
 
 
 const StudyCases = () => {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,        
+        responsive: [
+
+        {
+            breakpoint: 768,
+            settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            initialSlide: 0
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+            }
+        }
+        ]
+    }
     return (        
     <div className="container px-5 mx-auto">
         <h2 className="mt-4">Check out our solutions</h2>
@@ -12,8 +39,10 @@ const StudyCases = () => {
                 <div className="p-4 md:w-1/3">                
                     <div className="h-full border-2 border-gray-200 border-opacity-60 overflow-hidden mx-4 md:mx-0">
                         <img className="lg:h-48 md:h-36 w-full object-cover object-center" src={DATA.CASE1.img} alt={DATA.CASE1.alt} />
-                        <div className="p-4">
-                            {/* <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2> */}
+                        <div className="bg-secondary2 h-8 flex justify-start items-center">
+                            <h2 className="tracking-widest text-sm font-semibold text-white ml-2">CATEGORY</h2>
+                        </div>  
+                        <div className="p-4">                                                      
                             <h1 className="title-font text-lg font-bold text-primary mb-3">{DATA.CASE1.title}</h1>
                             <p className="mt-2 text-base leading-6 text-gray-500">{DATA.CASE1.description}</p>
                             <div className="flex items-center flex-wrap ">
@@ -30,8 +59,10 @@ const StudyCases = () => {
                 <div className="p-4 md:w-1/3">                
                     <div className="h-full border-2 border-gray-200 border-opacity-60 overflow-hidden mx-4 md:mx-0">
                         <img className="lg:h-48 md:h-36 w-full object-cover object-center" src={DATA.CASE1.img} alt={DATA.CASE1.alt} />
-                        <div className="p-4">
-                            {/* <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2> */}
+                        <div className="bg-secondary2 h-8 flex justify-start items-center">
+                            <h2 className="tracking-widest text-sm font-semibold text-white ml-2">CATEGORY</h2>
+                        </div>
+                        <div className="p-4">                           
                             <h1 className="title-font text-lg font-bold text-primary mb-3">{DATA.CASE1.title}</h1>
                             <p className="mt-2 text-base leading-6 text-gray-500">{DATA.CASE1.description}</p>
                             <div className="flex items-center flex-wrap ">
@@ -48,8 +79,10 @@ const StudyCases = () => {
                 <div className="p-4 md:w-1/3">                
                     <div className="h-full border-2 border-gray-200 border-opacity-60 overflow-hidden mx-4 md:mx-0">
                         <img className="lg:h-48 md:h-36 w-full object-cover object-center" src={DATA.CASE1.img} alt={DATA.CASE1.alt} />
-                        <div className="p-4">
-                            {/* <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2> */}
+                        <div className="bg-secondary2 h-8 flex justify-start items-center">
+                            <h2 className="tracking-widest text-sm font-semibold text-white ml-2">CATEGORY</h2>
+                        </div>
+                        <div className="p-4">                            
                             <h1 className="title-font text-lg font-bold text-primary mb-3">{DATA.CASE1.title}</h1>
                             <p className="mt-2 text-base leading-6 text-gray-500">{DATA.CASE1.description}</p>
                             <div className="flex items-center flex-wrap ">
@@ -64,7 +97,77 @@ const StudyCases = () => {
                     </div>
                 </div>            
             </div>
-        </div>      
+        </div> 
+        <div className="md:hidden">
+            <Slider {...settings}> 
+                <div>
+                    <div className="p-4 md:w-1/3">                
+                        <div className="h-full border-2 border-gray-200 border-opacity-60 overflow-hidden mx-4 md:mx-0">
+                            <img className="lg:h-48 md:h-36 w-full object-cover object-center" src={DATA.CASE1.img} alt={DATA.CASE1.alt} />
+                            <div className="bg-secondary2 h-8 flex justify-start items-center">
+                                <h2 className="tracking-widest text-sm font-semibold text-white ml-2">CATEGORY</h2>
+                            </div>  
+                            <div className="p-4">                                                      
+                                <h1 className="title-font text-lg font-bold text-primary mb-3">{DATA.CASE1.title}</h1>
+                                <p className="mt-2 text-base leading-6 text-gray-500">{DATA.CASE1.description}</p>
+                                <div className="flex items-center flex-wrap ">
+                                    <a href="" className="text-secondary2 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+                                        <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                        <path d="M5 12h14"></path>
+                                        <path d="M12 5l7 7-7 7"></path>
+                                        </svg>
+                                    </a>
+                                </div>
+                            </div>    
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className="p-4 md:w-1/3">                
+                        <div className="h-full border-2 border-gray-200 border-opacity-60 overflow-hidden mx-4 md:mx-0">
+                            <img className="lg:h-48 md:h-36 w-full object-cover object-center" src={DATA.CASE1.img} alt={DATA.CASE1.alt} />
+                            <div className="bg-secondary2 h-8 flex justify-start items-center">
+                                <h2 className="tracking-widest text-sm font-semibold text-white ml-2">CATEGORY</h2>
+                            </div>  
+                            <div className="p-4">                                                      
+                                <h1 className="title-font text-lg font-bold text-primary mb-3">{DATA.CASE1.title}</h1>
+                                <p className="mt-2 text-base leading-6 text-gray-500">{DATA.CASE1.description}</p>
+                                <div className="flex items-center flex-wrap ">
+                                    <a href="" className="text-secondary2 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+                                        <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                        <path d="M5 12h14"></path>
+                                        <path d="M12 5l7 7-7 7"></path>
+                                        </svg>
+                                    </a>
+                                </div>
+                            </div>    
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className="p-4 md:w-1/3">                
+                        <div className="h-full border-2 border-gray-200 border-opacity-60 overflow-hidden mx-4 md:mx-0">
+                            <img className="lg:h-48 md:h-36 w-full object-cover object-center" src={DATA.CASE1.img} alt={DATA.CASE1.alt} />
+                            <div className="bg-secondary2 h-8 flex justify-start items-center">
+                                <h2 className="tracking-widest text-sm font-semibold text-white ml-2">CATEGORY</h2>
+                            </div>  
+                            <div className="p-4">                                                      
+                                <h1 className="title-font text-lg font-bold text-primary mb-3">{DATA.CASE1.title}</h1>
+                                <p className="mt-2 text-base leading-6 text-gray-500">{DATA.CASE1.description}</p>
+                                <div className="flex items-center flex-wrap ">
+                                    <a href="" className="text-secondary2 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+                                        <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                        <path d="M5 12h14"></path>
+                                        <path d="M12 5l7 7-7 7"></path>
+                                        </svg>
+                                    </a>
+                                </div>
+                            </div>    
+                        </div>
+                    </div>
+                </div>                           
+            </Slider>
+        </div>     
 
     </div>    
         
--- src/components/Tech/index.js
+++ src/components/Tech/index.js
@@ -12,38 +12,38 @@ import sklearn from "../../assets/images/technologies/sklearn.png"
 
 const Tech = () => {
     return (
-        <div class="container px-5 mx-auto">
-            <h2 class="mt-4">Lorem ipsum dolor sit amet consectetur. </h2>
-            <h4 class="mb-6">Blanditiis enim nihil earum qui, a non fugit consectetur nobis natus quasi...</h4>
-            <div class="flex flex-wrap -m-4 sm:mt-1/4 lg:mt-0">
-                <div class="p-4 w-1/2 sm:w-1/5">
+        <div className="container px-5 mx-auto">
+            <h2 className="mt-4">Lorem ipsum dolor sit amet consectetur. </h2>
+            <h4 className="mb-6">Blanditiis enim nihil earum qui, a non fugit consectetur nobis natus quasi...</h4>
+            <div className="flex flex-wrap -m-4 sm:mt-1/4 lg:mt-0">
+                <div className="p-4 w-1/2 sm:w-1/5">
                     <img src={altair} alt="Altair" />
                 </div>
-                <div class="p-4 w-1/2 sm:w-1/5">
+                <div className="p-4 w-1/2 sm:w-1/5">
                     <img src={aws} alt="Amazon Web Services" />
                 </div>
-                <div class="p-4 w-1/2 sm:w-1/5">
+                <div className="p-4 w-1/2 sm:w-1/5">
                     <img src={bokeh} alt="Bokeh" />
                 </div>
-                <div class="p-4 w-1/2 sm:w-1/5">
+                <div className="p-4 w-1/2 sm:w-1/5">
                     <img src={chalice} alt="Chalice" />
                 </div>
-                <div class="p-4 w-1/2 sm:w-1/5">
+                <div className="p-4 w-1/2 sm:w-1/5">
                     <img src={keras} alt="Keras" />
                 </div>
-                <div class="p-4 w-1/2 sm:w-1/5">
+                <div className="p-4 w-1/2 sm:w-1/5">
                     <img src={mat} alt="Matplotlib" />
                 </div>
-                <div class="p-4 w-1/2 sm:w-1/5">
+                <div className="p-4 w-1/2 sm:w-1/5">
                     <img src={nuxt} alt="Nuxtjs" />
                 </div>
-                <div class="p-4 w-1/2 sm:w-1/5">
+                <div className="p-4 w-1/2 sm:w-1/5">
                     <img src={python} alt="Python" />
                 </div>
-                <div class="p-4 w-1/2 sm:w-1/5">
+                <div className="p-4 w-1/2 sm:w-1/5">
                     <img src={sage} alt="Sagemaker" />
                 </div>
-                <div class="p-4 w-1/2 sm:w-1/5">
+                <div className="p-4 w-1/2 sm:w-1/5">
                     <img src={sklearn} alt="Sklearn" />
                 </div>
             </div>
