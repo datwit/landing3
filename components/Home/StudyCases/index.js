88e5c6b2a488a86afcd175b26d998fa84bb694cf
Enma Lidia Munoz Garcia <enmalidia88@gmail.com>
2021-03-24 10:00:39 -0400

    style: :lipstick: Adding styles components: study cases section
    #45

--- components/Home/StudyCases/index.js
+++ components/Home/StudyCases/index.js
@@ -1,39 +1,39 @@
 import datas from "./data.json";
+import {SCardWrapper, SCardBorder, CategoryRect, CategoryTitle, CardContent, CardTitle, CardSubtitle, ReadMore} from './style';
+import {ContentWrapper, SectionSubheader, SectionHeader} from '../../../styles/global';
 
 
 const StudyCases = () => {
     
     return (        
     <div className="container px-5 mx-auto">
-        <h2 className="mt-4">Check out our solutions</h2>
-        <h4 className="mb-6">Commitment and creativity mark our creations. By all means, stroll along...</h4>
+        <SectionHeader>Check out our solutions</SectionHeader>
+        <SectionSubheader>Commitment and creativity mark our creations. By all means, stroll along...</SectionSubheader>
         <div className="hidden md:block">
-            <div className="flex flex-wrap -m-4">    
+            <ContentWrapper>    
                {
                     datas.map((data, key) => (
-                        <div className="p-4 md:w-1/3" key={key}>                
-                            <div className="h-full border-2 border-gray-200 border-opacity-60 overflow-hidden mx-4 md:mx-0">
+                        <SCardWrapper key={key}>                
+                            <SCardBorder>
                                 <img className="lg:h-48 md:h-36 w-full object-cover object-center" src={data.src} alt={data.alt} />
-                                <div className="bg-secondary2 h-8 flex justify-start items-center">
-                                    <h2 className="tracking-widest text-sm font-semibold text-white ml-2">CATEGORY</h2>
-                                </div>  
-                                <div className="p-4">                                                      
-                                    <h1 className="title-font text-lg font-bold text-primary mb-3">{data.title}</h1>
-                                    <p className="mt-2 text-base leading-6 text-gray-500">{data.description}</p>
-                                    <div className="flex items-center flex-wrap ">
-                                        <a href="" className="text-secondary2 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+                                <CategoryRect>
+                                    <CategoryTitle>CATEGORY</CategoryTitle>
+                                </CategoryRect>  
+                                <CardContent>                                                      
+                                    <CardTitle>{data.title}</CardTitle>
+                                    <CardSubtitle>{data.description}</CardSubtitle>                                    
+                                        <ReadMore href="" >Learn More
                                             <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                            <path d="M5 12h14"></path>
-                                            <path d="M12 5l7 7-7 7"></path>
+                                                <path d="M5 12h14"></path>
+                                                <path d="M12 5l7 7-7 7"></path>
                                             </svg>
-                                        </a>
-                                    </div>
-                                </div>    
-                            </div>
-                        </div> 
+                                        </ReadMore>                                    
+                                </CardContent>    
+                            </SCardBorder>
+                        </SCardWrapper> 
                     ))
                }                    
-            </div>
+            </ContentWrapper>
         </div> 
         <div className="md:hidden">                                      
          {/**********responsive************** */}
