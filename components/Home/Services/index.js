5bf60a69fc2bca334026c035fbb719f063878164
Enma Lidia Munoz Garcia <enmalidia88@gmail.com>
2021-03-24 09:23:01 -0400

    style: :lipstick: Adding styled components: services section
    #45

--- components/Home/Services/index.js
+++ components/Home/Services/index.js
@@ -1,5 +1,7 @@
 import datas from "./data.json";
 import {useState} from 'react'
+import {ContentWrapper, SectionSubheader}from '../../../styles/global';
+import {IconWrapper, IconListWrapper, IconListItem, IconTitle, TabContent, TabContentInner, BulletTitle, BulletSubtitle, ServiceBulletsOuter, ServiceBulletsSpan} from './styles';
 
 
 const Services = () => {
@@ -18,50 +20,50 @@ const Services = () => {
    
     return (
         <div className="container px-5 mx-auto">
-            <h4 className="mb-6">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h4>
-            <div className="flex flex-wrap -m-4">
-                <div className="sm:w-1/4 h-full w-full">
-                    <ul className="flex mb-0 list-none flex-wrap pt-3 pb-4 flex-row sm:flex-col">
-                        <li className="-mb-px mr-2 flex-auto text-center flex items-center sm:pb-12 flex-col">
-                        <svg className={`${tab1 ? 'text-secondary2':'text-primary'} h-8 w-8`}  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"
-                            >
-                            <polyline points="16 18 22 12 16 6" />  <polyline points="8 6 2 12 8 18" />
-                        </svg>
-                        <a className={`${tab1 ? 'text-secondary2':'text-primary'} text-base font-bold  block leading-normal relative cursor-pointer`} onClick = {handleClick1}>
+            <SectionSubheader>Lorem ipsum dolor sit amet consectetur adipisicing elit.</SectionSubheader>
+            <ContentWrapper>
+                <IconWrapper>
+                    <IconListWrapper>
+                        <IconListItem>
+                            <svg className={`${tab1 ? 'text-secondary2':'text-primary'} h-8 w-8`}  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round">
+                                <polyline points="16 18 22 12 16 6" />  <polyline points="8 6 2 12 8 18" />
+                            </svg>
+                        <IconTitle className={tab1 ? 'text-secondary2':'text-primary'} onClick = {handleClick1}>
                             Software
                             <br />
                             Development
-                        </a>
-                        </li>
-                        <li className="-mb-px mr-2 flex-auto text-center flex items-center flex-col">
-                        <svg className={`${tab2 ? 'text-secondary2':'text-primary'} h-8 w-8`}  width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"
-                            >  <path stroke="none" d="M0 0h24v24H0z"/>
-                            <ellipse cx="12" cy="6" rx="8" ry="3"></ellipse>  <path d="M4 6v6a8 3 0 0 0 16 0v-6" />
-                            <path d="M4 12v6a8 3 0 0 0 16 0v-6" />
-                        </svg>
-                        <a className={`${tab2 ? 'text-secondary2':'text-primary'} text-base font-bold  block leading-normal relative cursor-pointer`} onClick = {handleClick2}>
+                        </IconTitle>
+                        </IconListItem>
+                        <IconListItem>
+                            <svg className={`${tab2 ? 'text-secondary2':'text-primary'} h-8 w-8`}  width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  
+                            <path stroke="none" d="M0 0h24v24H0z"/>
+                                <ellipse cx="12" cy="6" rx="8" ry="3"></ellipse>  <path d="M4 6v6a8 3 0 0 0 16 0v-6" />
+                                <path d="M4 12v6a8 3 0 0 0 16 0v-6" />
+                            </svg>
+                        <IconTitle  className={tab2 ? 'text-secondary2':'text-primary'} onClick = {handleClick2}>
                             Data
                             <br />
                             Science
-                        </a>
-                        </li>
-                    </ul>
-                </div>
-                <div className="sm:w-3/4 relative flex flex-col min-w-0 break-words bg-white w-full h-full mb-6 shadow-lg">
-                    <div className="px-4 flex-auto border-2 border-gray-200 border-opacity-60">                       
+                        </IconTitle>
+                        </IconListItem>
+                    </IconListWrapper>
+                </IconWrapper>
+                <TabContent>
+                    <TabContentInner>                       
                             <div className={tab1 ? 'block' : 'hidden'}>
                             {
                                 datas.soft.map((data, key) => (
                                     <div key={key}>
-                                        <h2 className= "text-lg mt-2 text-left">{data.title}</h2>
-                                        <p className="text-base mt-2">{data.subtitle}</p>
+                                        <BulletTitle>{data.title}</BulletTitle>
+                                        <BulletSubtitle>{data.subtitle}</BulletSubtitle>
                                     {
                                       data.items.map((data,key1)=>(
                                         <div  key={key1}> 
-                                        <p className="text-base mt-2 flex items-baseline">
-                                        <span className="w-5 h-5 mr-2 inline-flex items-center justify-center flex-shrink-0">
-                                        <svg className="h-8 w-8 text-secondary2"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round">  <circle cx="12" cy="12" r="10" />  <polyline points="12 16 16 12 12 8" />  <line x1="8" y1="12" x2="16" y2="12" /></svg>
-                                        </span>{data.tit}</p>
+                                            <ServiceBulletsOuter>
+                                                <ServiceBulletsSpan>
+                                                <svg className="h-8 w-8 text-secondary2"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round">  <circle cx="12" cy="12" r="10" />  <polyline points="12 16 16 12 12 8" />  <line x1="8" y1="12" x2="16" y2="12" /></svg>
+                                                </ServiceBulletsSpan>{data.tit}
+                                            </ServiceBulletsOuter>
                                         </div>
                                       ))
                                     }
@@ -74,15 +76,16 @@ const Services = () => {
                             {
                                 datas.data.map((data, key) => (
                                     <div  key={key}>
-                                    <h2 className= "text-lg mt-2 text-left">{data.title}</h2>
-                                    <p className="text-base mt-2">{data.subtitle}</p>
+                                    <BulletTitle>{data.title}</BulletTitle>
+                                    <BulletSubtitle>{data.subtitle}</BulletSubtitle>
                                     {
                                       data.items.map((data,key1)=>(
                                         <div  key={key1}>
-                                            <p className="text-base mt-2 flex items-baseline">
-                                            <span className="w-5 h-5 mr-2 inline-flex items-center justify-center flex-shrink-0">
-                                            <svg className="h-8 w-8 text-secondary2"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round">  <circle cx="12" cy="12" r="10" />  <polyline points="12 16 16 12 12 8" />  <line x1="8" y1="12" x2="16" y2="12" /></svg>
-                                            </span>{data.tit}</p>
+                                            <ServiceBulletsOuter>
+                                                <ServiceBulletsSpan>
+                                                <svg className="h-8 w-8 text-secondary2"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round">  <circle cx="12" cy="12" r="10" />  <polyline points="12 16 16 12 12 8" />  <line x1="8" y1="12" x2="16" y2="12" /></svg>
+                                                </ServiceBulletsSpan>{data.tit}
+                                            </ServiceBulletsOuter>
                                         </div>  
                                       ))
                                     }
@@ -90,9 +93,9 @@ const Services = () => {
                                 ))
                             }
                             </div>                       
-                    </div>
-                </div>
-            </div>
+                    </TabContentInner>
+                </TabContent>
+            </ContentWrapper>
         </div>       
         
     )
--- components/Home/Services/styles.js
+++ components/Home/Services/styles.js
@@ -1,29 +1,33 @@
 import styled from 'styled-components'
 
-export const PageBody = styled("div")`
-  width: 100%;
-  height: 100%;
-  padding: 2em;
-`
-export const TabHead = styled("div")`
-  border-bottom: 1px solid black;
-  display: flex;
-  background: black;
-`
-export const TabContainer = styled("div")`
-  width: 30em;
-  height: 30em;
-  webkit-box-shadow: -1px 0px 5px 0px rgba(184, 184, 184, 1);
-  -moz-box-shadow: -1px 0px 5px 0px rgba(184, 184, 184, 1);
-  box-shadow: -1px 0px 5px 0px rgba(184, 184, 184, 1);
-`
-export const TabBody = styled(PageBody)`
-  height: 100%;
-`
-export const Tab = styled("div")`
-  padding: 1em;
-  background: ${({ selected }) => (selected ? "grey" : "black")};
-  * {
-    color: white;
-  }
-`
+
+export const IconWrapper = styled.div.attrs({
+  className: "sm:w-1/4 h-full w-full"
+})``;
+export const IconListWrapper = styled.ul.attrs({
+  className: "flex mb-0 list-none flex-wrap pt-3 pb-4 flex-row sm:flex-col"
+})``;
+export const IconListItem = styled.li.attrs({
+  className: "-mb-px mr-2 flex-auto text-center flex items-center sm:pb-12 flex-col"
+})``;
+export const IconTitle = styled.a.attrs({
+  className: "text-base font-bold  block leading-normal relative cursor-pointer"
+})``;
+export const TabContent = styled.div.attrs({
+  className: "sm:w-3/4 relative flex flex-col min-w-0 break-words bg-white w-full h-full mb-6 shadow-lg"
+})``;
+export const TabContentInner = styled.div.attrs({
+  className: "px-4 flex-auto border-2 border-gray-200 border-opacity-60"
+})``;
+export const BulletTitle = styled.h2.attrs({
+  className: "text-lg mt-2 text-left"
+})``;
+export const BulletSubtitle = styled.p.attrs({
+  className: "text-base mt-2"
+})``;
+export const ServiceBulletsOuter = styled.p.attrs({
+  className: "text-base mt-2 flex items-baseline"
+})``;
+export const ServiceBulletsSpan = styled.span.attrs({
+  className: "w-5 h-5 mr-2 inline-flex items-center justify-center flex-shrink-0"
+})``;
