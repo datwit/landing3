5b0eddbe861f086c3b9ff56794406341b1244809
Enma Lidia Munoz Garcia <enmalidia88@gmail.com>
2021-03-24 11:51:10 -0400

    feat: :sparkles: Migrating page Team members to Nextjs
    #44

---
+++ components/Team/Tech/index.js
@@ -0,0 +1,43 @@
+
+const Tech = () => {
+    return (
+        <div className="container px-5 mx-auto">
+            <h2 className="mt-4">Lorem ipsum dolor sit amet consectetur. </h2>
+            <h4 className="mb-6">Blanditiis enim nihil earum qui, a non fugit consectetur nobis natus quasi...</h4>
+            <div className="flex flex-wrap -m-4 sm:mt-1/4 lg:mt-0">
+                <div className="p-4 w-1/2 sm:w-1/5">
+                    <img src="/images/technologies/altair.png" alt="Altair"/>
+                </div>
+                <div className="p-4 w-1/2 sm:w-1/5">
+                    <img src="/images/technologies/aws.png" alt="Amazon Web Services"/>
+                </div>
+                <div className="p-4 w-1/2 sm:w-1/5">
+                    <img src="/images/technologies/bokeh.png" alt="Bokeh"/>
+                </div>
+                <div className="p-4 w-1/2 sm:w-1/5">
+                    <img src="/images/technologies/chalice.png" alt="Chalice"/>
+                </div>
+                <div className="p-4 w-1/2 sm:w-1/5">
+                    <img src="/images/technologies/keras.png" alt="Keras"/>
+                </div>
+                <div className="p-4 w-1/2 sm:w-1/5">
+                    <img src="/images/technologies/matplotlib.png" alt="Matplotlib"/>
+                </div>
+                <div className="p-4 w-1/2 sm:w-1/5">
+                    <img src="/images/technologies/chalice.png" alt="Nuxtjs"/>
+                </div>
+                <div className="p-4 w-1/2 sm:w-1/5">
+                    <img src="/images/technologies/python.png" alt="Python"/>
+                </div>
+                <div className="p-4 w-1/2 sm:w-1/5">
+                    <img src="/images/technologies/sagemaker.png" alt="Sagemaker"/>
+                </div>
+                <div className="p-4 w-1/2 sm:w-1/5">
+                    <img src="/images/technologies/sklearn.png" alt="Sklearn"/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Tech
