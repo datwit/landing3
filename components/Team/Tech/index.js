27f1497abb24d995d788645066ea5caa465a1714
Enma Lidia Muñoz Garcia <enmalidia88@gmail.com>
2021-03-28 02:05:04 -0600

    refactor: :recycle: Tech section: Adding styled components. Adding data source
    #45

--- components/Team/Tech/index.js
+++ components/Team/Tech/index.js
@@ -1,41 +1,24 @@
 
+import {SectionHeader, SectionSubheader} from '../../../styles/global'
+import datas from './data.json'
+import {ImgContainer, ImgWrapper} from './style'
+
+
 const Tech = () => {
     return (
         <div className="container px-5 mx-auto">
-            <h2 className="mt-4">Lorem ipsum dolor sit amet consectetur. </h2>
-            <h4 className="mb-6">Blanditiis enim nihil earum qui, a non fugit consectetur nobis natus quasi...</h4>
-            <div className="flex flex-wrap -m-4 sm:mt-1/4 lg:mt-0">
-                <div className="p-4 w-1/2 sm:w-1/5">
-                    <img src="/images/technologies/altair.png" alt="Altair"/>
-                </div>
-                <div className="p-4 w-1/2 sm:w-1/5">
-                    <img src="/images/technologies/aws.png" alt="Amazon Web Services"/>
-                </div>
-                <div className="p-4 w-1/2 sm:w-1/5">
-                    <img src="/images/technologies/bokeh.png" alt="Bokeh"/>
-                </div>
-                <div className="p-4 w-1/2 sm:w-1/5">
-                    <img src="/images/technologies/chalice.png" alt="Chalice"/>
-                </div>
-                <div className="p-4 w-1/2 sm:w-1/5">
-                    <img src="/images/technologies/keras.png" alt="Keras"/>
-                </div>
-                <div className="p-4 w-1/2 sm:w-1/5">
-                    <img src="/images/technologies/matplotlib.png" alt="Matplotlib"/>
-                </div>
-                <div className="p-4 w-1/2 sm:w-1/5">
-                    <img src="/images/technologies/chalice.png" alt="Nuxtjs"/>
-                </div>
-                <div className="p-4 w-1/2 sm:w-1/5">
-                    <img src="/images/technologies/python.png" alt="Python"/>
-                </div>
-                <div className="p-4 w-1/2 sm:w-1/5">
-                    <img src="/images/technologies/sagemaker.png" alt="Sagemaker"/>
-                </div>
-                <div className="p-4 w-1/2 sm:w-1/5">
-                    <img src="/images/technologies/sklearn.png" alt="Sklearn"/>
-                </div>
-            </div>
+            <SectionHeader>Lorem ipsum dolor sit amet consectetur. </SectionHeader>
+            <SectionSubheader>Blanditiis enim nihil earum qui, a non fugit consectetur nobis natus quasi...</SectionSubheader>
+            <ImgContainer>
+                {
+                    datas.map((data,key)=>(
+                        <ImgWrapper key={key}>
+                            <img src={data.src} alt={data.alt}/>
+                        </ImgWrapper>
+
+                    ))
+                }                
+            </ImgContainer>
         </div>
     )
 }
