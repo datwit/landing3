8c994a6897cd7a2fd73cae0924fb0355171a57e7
Enma Lidia Muñoz Garcia <enmalidia88@gmail.com>
2021-03-27 22:36:43 -0600

    refactor: :recycle: Find partnership section: Adding styled components. Adding data source
    #45

--- components/Partner/Find/index.js
+++ components/Partner/Find/index.js
@@ -1,45 +1,29 @@
-
+import {ContentWrapper, SectionHeader, SectionSubheader} from '../../../styles/global'
+import {IconBlock, IconContent, IconTitle, IconText} from './style'
+import {SCardWrapper} from '../../Home/StudyCases/style'
+import datas from './data.json'
 
 const Find = () => {
     return (
         <div className="container px-5 mx-auto">
-            <h2 className="mt-0">Find the right partnership</h2>
-            <h4 className="mb-6">Lorem, ipsum dolor sit amet consectetur adipisicing elit...</h4>
-                <div className="flex flex-wrap -m-4">
-                    <div className="md:p-4 sm:w-1/3">
-                        <div>
-                            <div className="h-full overflow-hidden mx-4 md:mx-0">
-                                <img className="lg:h-48 md:h-36 w-full object-cover object-center px-8" src="" alt="img" />
-                                <div className="md:p-4 text-center">
-                                    <h1 className="title-font text-sm sm:text-lg font-bold text-primary md:mb-3">Lorem ipsum dolor si amet</h1>
-                                    <p className="text-sm sm:text-base leading-5 text-gray-500 mb-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam consequuntur facilis</p>
-                                </div>
-                            </div>
-                        </div>                        
-                    </div>
-                    <div className="md:p-4 sm:w-1/3">
-                        <div>
-                            <div className="h-full overflow-hidden mx-4 md:mx-0">
-                                <img className="lg:h-48 md:h-36 w-full object-cover object-center px-8" src="" alt="img" />
-                                <div className="md:p-4 text-center">
-                                    <h1 className="title-font text-sm sm:text-lg font-bold text-primary md:mb-3">Lorem ipsum dolor si amet</h1>
-                                    <p className="text-sm sm:text-base leading-5 text-gray-500 mb-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam consequuntur facilis</p>
-                                </div>
-                            </div>
-                        </div>                        
-                    </div>
-                    <div className="md:p-4 sm:w-1/3">
-                        <div>
-                            <div className="h-full overflow-hidden mx-4 md:mx-0">
-                                <img className="lg:h-48 md:h-36 w-full object-cover object-center px-8" src="" alt="img" />
-                                <div className="md:p-4 text-center">
-                                    <h1 className="title-font text-sm sm:text-lg font-bold text-primary md:mb-3">Lorem ipsum dolor si amet</h1>
-                                    <p className="text-sm sm:text-base leading-5 text-gray-500 mb-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam consequuntur facilis</p>
-                                </div>
-                            </div>
-                        </div>                        
-                    </div>                    
-                </div>
+            <SectionHeader>Find the right partnership</SectionHeader>
+            <SectionSubheader>Lorem, ipsum dolor sit amet consectetur adipisicing elit...</SectionSubheader>
+                <ContentWrapper>
+                    {
+                        datas.map((data,key)=>(
+                            <SCardWrapper key={key}>                       
+                                <IconBlock>
+                                    <img className="lg:h-48 md:h-36 w-full object-cover object-center px-8" src="" alt="img" />
+                                    <IconContent>
+                                        <IconTitle>Lorem ipsum dolor si amet</IconTitle>
+                                        <IconText>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam consequuntur facilis</IconText>
+                                    </IconContent>
+                                </IconBlock>                                              
+                            </SCardWrapper>
+
+                        ))
+                    }                                   
+                </ContentWrapper>
         </div>
     )
 }
--- styles/global.js
+++ styles/global.js
@@ -4,7 +4,7 @@ export const ContentWrapper = styled.div.attrs({
     className: "flex flex-wrap -m-4"
 })``;
 export const SectionHeader = styled.h2.attrs({
-    className: "fmt-4"
+    className: "mt-4"
 })``;
 export const SectionSubheader = styled.h4.attrs({
     className: "mb-6"
