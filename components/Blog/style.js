dd7a39f9ef9b85436393a39c96645ec5de7197d3
Enma Lidia Munoz Garcia <enmalidia88@gmail.com>
2021-03-29 11:06:24 -0400

    refactor: :recycle: Adding styled cmponents and cleanning
    #47

--- components/Blog/style.js
+++ components/Blog/style.js
@@ -9,4 +9,17 @@ export const DropdownWrapper = styled.div.attrs({
 export const SearchWrapper = styled.div.attrs({
     className: "flex items-center justify-end"
 })``;
+export const BlogCardWrapper = styled.div.attrs({
+    className: "p-4 md:w-1/3 w-full hidden md:block"
+})``;
+export const BlogCardBlock = styled.div.attrs({
+    className: "p-3"
+})``;
+export const CardSummary = styled.p.attrs({
+    className: "mt-0 text-base leading-6 text-gray-500"
+})``;
+export const BlogCardBorder = styled.div.attrs({
+    className: "h-full border border-gray-300 shadow hover:shadow-lg transition duration-200 ease-in overflow-hidden mx-4 md:mx-0"
+})``;
+
 
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -6,7 +6,7 @@ import { getAllPosts } from './GetPosts';
 import { format, parseISO } from 'date-fns';
 import CustomControls from '../../components/Slide/CustomControls'
 import {SectionSubheader, ContentWrapper} from '../../styles/global'
-import {UpperRowBlog, DropdownWrapper, SearchWrapper} from '../../components/Blog/style'
+import {UpperRowBlog, DropdownWrapper, SearchWrapper, BlogCardWrapper, BlogCardBlock, CardSummary, BlogCardBorder} from '../../components/Blog/style'
 
 
 
@@ -25,7 +25,7 @@ const Blog = ({ posts }) =>{
                 </DropdownWrapper>
                 {/* search box*/}
                 <SearchWrapper>
-                    <input type="text" name="search" placeholder="Search here" className="w-3/4 rounded border border-gray-300 bg-white  text-base outline-none text-gray-700 px-3 leading-8 focus:border-primary focus:ring-2 focus:ring-indigo-200 transition-colors duration-200 ease-in-out"/>
+                    <input type="text" name="search" placeholder="Search here" className="w-3/4 rounded border border-gray-300 bg-white  text-base outline-none text-gray-700 px-3 leading-8 focus:ring-2 focus:ring-secondary2 transition-colors duration-200 ease-in-out"/>
                     <svg className="h-6 w-6 text-gray-700 opacity-40"  width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <circle cx="10" cy="10" r="7" />  <line x1="21" y1="21" x2="15" y2="15" /></svg>
                 </SearchWrapper>
             </UpperRowBlog>              
@@ -83,27 +83,29 @@ const BlogListItem = ({ slug, title, date, summary })=> {
   
   return (
     <>        
-      <div className="p-4 md:w-1/3 w-full hidden md:block">
-          <Link href={`/blog/${slug}`}><div className="h-full border border-gray-300 shadow hover:shadow-lg  transition duration-200 ease-in overflow-hidden mx-4 md:mx-0">
-              <img className="lg:h-48 md:h-28 w-full object-cover object-center" src="" alt="" />              
-              <div className="p-3">
-                  <div className="text-gray-600 text-xs">
-                      {format(parseISO(date), 'MMMM do, uuu')}
-                  </div>
-                  {/* <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2> */}
-                  <Link href={`/blog/${slug}`}><h1 className="title-font text-lg font-bold text-primary mb-1 cursor-pointer">{title}</h1></Link> 
-                  <p className="mt-0 text-base leading-6 text-gray-500">{summary}</p>                  
-                  {/* <div className="flex items-center flex-wrap ">
-                      <a className="text-secondary2 inline-flex items-center md:mb-2 lg:mb-0">Learn More
-                          <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                              <path d="M5 12h14"></path>
-                              <path d="M12 5l7 7-7 7"></path>
-                          </svg>
-                      </a>
-                  </div> */}
-              </div>
-          </div></Link>
-      </div>
+      <BlogCardWrapper>
+          <Link href={`/blog/${slug}`}>
+              <BlogCardBorder>
+                  <img className="lg:h-48 md:h-28 w-full object-cover object-center" src="/images/dummy-image.png" alt="" />              
+                  <BlogCardBlock>
+                      <div className="text-gray-600 text-xs">
+                          {format(parseISO(date), 'MMMM do, uuu')}
+                      </div>
+                      {/* <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2> */}
+                      <Link href={`/blog/${slug}`}><h1 className="title-font text-lg font-bold text-primary mb-1 cursor-pointer">{title}</h1></Link> 
+                      <CardSummary>{summary}</CardSummary>                  
+                      {/* <div className="flex items-center flex-wrap ">
+                          <a className="text-secondary2 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+                              <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                  <path d="M5 12h14"></path>
+                                  <path d="M12 5l7 7-7 7"></path>
+                              </svg>
+                          </a>
+                      </div> */}
+                  </BlogCardBlock>
+              </BlogCardBorder>
+          </Link>
+      </BlogCardWrapper>
       <div className="md:hidden">
           <Link href={`/blog/${slug}`}>
               <div className="border border-gray-300 shadow hover:shadow-lg transition duration-200 ease-in mb-3 p-3 cursor-pointer">
