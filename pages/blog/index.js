93c543da257868d408306c7b98d94fba661c2c57
Enma Lidia Munoz Garcia <enmalidia88@gmail.com>
2021-04-05 16:26:09 -0400

    feat: :construction: Blog posts page: testing page navigation
    #49

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -2,13 +2,13 @@ import { FullPage, Slide } from 'react-full-page'
 import Section from '../../components/Section'
 import Footer from '../../components/Footer'
 import Link from "next/link"
-import { useCallback, useRef, useState, Fragment } from 'react'
+import { useCallback, useRef, useState, Fragment, useEffect } from 'react'
 import { getAllData } from '../../lib/posts'
 import { format, parseISO } from 'date-fns'
 import CustomControls from '../../components/Slide/CustomControls'
 import {SectionSubheader, ContentWrapper} from '../../styles/global'
-import {SearchWrapper, BlogCardWrapper, BlogCardBlock, CardSummary, BlogCardBorder, RespBlock,BlogTitle1, BlogTitle2, DateWrapper, Category} from '../../components/Blog/style'
-
+import {Pagination,SearchWrapper, BlogCardWrapper, BlogCardBlock, CardSummary, BlogCardBorder, RespBlock,BlogTitle1, BlogTitle2, DateWrapper, Category} from '../../components/Blog/style'
+import ReactPaginate from 'react-paginate';
 
 import posts from '../../cache/data.json'
 
@@ -18,6 +18,72 @@ const Blog = () =>{
   const [queryCat, setQueryCat] = useState('')
   const [active, setActive] = useState(false)
   const [results, setResults] = useState([])
+
+  //pagination states
+  const [offset, setOffset] = useState(0);
+  const [data, setData] = useState([]);
+  const [perPage] = useState(3);
+  const [pageCount, setPageCount] = useState(0)
+
+  
+  //getting our data 
+  const getData = () => {
+    const data=posts
+    console.log("d:", posts)
+  //slicing data   
+    const slice = data.slice(offset, offset + perPage)
+    if (results.length == 0){
+      const postData = slice.map((item, key6) =>         
+            <BlogCardWrapper key={key6}>                               
+                <BlogCardBorder>
+                    <img className="lg:h-48 md:h-28 w-full object-cover object-center" src={item.img} alt="" />              
+                    <BlogCardBlock>
+                        <div className="text-gray-600 text-xs">
+                            {format(parseISO(item.date), 'MMMM do, uuu')}
+                        </div>
+                        <div className="flex flex-wrap">
+                          { item.tags.map((tags, key3) => (
+                            <Category                                                
+                              key={key3}                                                
+                              id={tags}
+                              onClick={onClickCat}
+                              >{tags}
+                            </Category>
+                            ))
+                          }
+                        </div>
+                        
+                        {/* <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2> */}
+                        <Link href={`/blog/${item.id}`}><BlogTitle1>{item.title}</BlogTitle1></Link> 
+                        <CardSummary>{item.summary}</CardSummary>                  
+                        {/* <div className="flex items-center flex-wrap ">
+                            <a className="text-secondary2 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+                                <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                    <path d="M5 12h14"></path>
+                                    <path d="M12 5l7 7-7 7"></path>
+                                </svg>
+                            </a>
+                        </div> */}
+                    </BlogCardBlock> 
+                </BlogCardBorder>                                                                
+            </BlogCardWrapper>           
+    )
+    setData(postData)
+    setPageCount(Math.ceil(data.length / perPage))
+      
+    }
+    
+  }
+  //callingData
+  useEffect(() => {
+    getData()
+  }, [offset])
+
+  const handlePageClick = (e) => {
+    const selectedPage = e.selected;
+    setOffset(selectedPage * perPage)
+};
+  
 
   const searchEndpoint = (queryEnd, params) => `/api/search?q=${queryEnd}&p=${params}`
 
@@ -89,34 +155,28 @@ const Blog = () =>{
                     />
                     <svg className="h-6 w-6 text-gray-700 opacity-40"  width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <circle cx="10" cy="10" r="7" />  <line x1="21" y1="21" x2="15" y2="15" /></svg>
                 </SearchWrapper>
-            
-            { results.length > 0 && (
-                <div>
-                  <ul>
-                    {
-                      results.map(({ id, title, summary, date, img },key1) => (
-                      <li key={key1}>                        
-                        <div className="mx-4 md:mx-0 mb-3">
-                          <div className="flex">
-                              <img src={img}  className="sm:w-1/5 w-1/2"  alt=""/>
-                              <RespBlock>                                  
-                                  <BlogTitle2>{title}</BlogTitle2>                                                    
-                                  <DateWrapper>
-                                      {format(parseISO(date), 'MMMM do, uuu')}
-                                  </DateWrapper>
-                                  <CardSummary className="hidden sm:block">{summary}</CardSummary>
-                                </RespBlock>  
-                          </div>                        
-                        </div>
-                      </li>
-                    ))
-                    }
-                  </ul>
-                </div>  
-              )
-            }   
 
-            {results.length == 0 && (
+                {/*pagination*/}
+                  <div className="App flex flex-wrap">
+                    {data}
+                    <ReactPaginate
+                      previousLabel={"prev"}
+                      nextLabel={"next"}
+                      breakLabel={"..."}
+                      breakClassName={"break-me"}
+                      pageCount={pageCount}
+                      marginPagesDisplayed={3}
+                      pageRangeDisplayed={3}
+                      onPageChange={handlePageClick}
+                      containerClassName={"pagination"}
+                      subContainerClassName={"pages pagination"}
+                      activeClassName={"active"}
+                        
+                      />
+                  </div>
+            
+            
+            {/* {results.length == 0 && (
                 <div>
                   <div className="hidden md:block">
                     <ContentWrapper>                
@@ -140,11 +200,11 @@ const Blog = () =>{
                                             </Category>
                                             ))
                                           }
-                                        </div>
+                                        </div> */}
                                         
                                         {/* <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2> */}
-                                        <Link href={`/blog/${item.id}`}><BlogTitle1>{item.title}</BlogTitle1></Link> 
-                                        <CardSummary>{item.summary}</CardSummary>                  
+                                        {/* <Link href={`/blog/${item.id}`}><BlogTitle1>{item.title}</BlogTitle1></Link> 
+                                        <CardSummary>{item.summary}</CardSummary>                   */}
                                         {/* <div className="flex items-center flex-wrap ">
                                             <a className="text-secondary2 inline-flex items-center md:mb-2 lg:mb-0">Learn More
                                                 <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
@@ -153,7 +213,7 @@ const Blog = () =>{
                                                 </svg>
                                             </a>
                                         </div> */}
-                                    </BlogCardBlock> 
+                                    {/* </BlogCardBlock> 
                                 </BlogCardBorder>                                                                
                             </BlogCardWrapper>
                             <div className="md:hidden">                                
@@ -190,9 +250,9 @@ const Blog = () =>{
                                                 {format(parseISO(item.date), 'MMMM do, uuu')}
                                             </div>
                                             
-                                            {/* <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2> */}
+                                            <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
                                             <Link href={`/blog/${item.id}`}><BlogTitle1>{item.title}</BlogTitle1></Link> 
-                                            <CardSummary>{item.summary}</CardSummary>                  
+                                            <CardSummary>{item.summary}</CardSummary>                   */}
                                             {/* <div className="flex items-center flex-wrap ">
                                                 <a className="text-secondary2 inline-flex items-center md:mb-2 lg:mb-0">Learn More
                                                     <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
@@ -201,7 +261,7 @@ const Blog = () =>{
                                                     </svg>
                                                 </a>
                                             </div> */}
-                                        </BlogCardBlock>                                        
+                                        {/* </BlogCardBlock>                                        
                                     </BlogCardBorder>                                                                   
                                 </BlogCardWrapper>
                                 <div className="md:hidden">                                    
@@ -224,9 +284,9 @@ const Blog = () =>{
                           ))
                           }                           
                       </ContentWrapper>
-                  </div>
-                </div>
-              )}
+                  </div>                  
+              </div>               
+              )} */}
                         
             </div>               
           </Section>        
