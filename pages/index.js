13cd654ba7b778a53ba6b8f51f1042ca5253f93e
Enma Lidia Muñoz Garcia <enmalidia88@gmail.com>
2021-04-11 16:10:08 -0500

    feat: :sparkles: Testing page transition on the homepage
    #51

--- pages/index.js
+++ pages/index.js
@@ -11,51 +11,62 @@ import Navbar from '../components/Navbar'
 import { motion } from 'framer-motion'
 
 export default function Home() {
-  const style={
-    height:'calc(100% - 80px)',    
-  } 
-  
+  const style = {
+    height: 'calc(100% - 80px)',
+  }
+
   return (
-    <motion.div initial={{opacity:0}} animate={{opacity:1}}> 
-        <FullPage controls={CustomControls}>        
-            <Slide {...style}>
-              <Navbar />
-              <Section>
-                <Hero />
-              </Section>        
-            </Slide>
+    <motion.div
+      initial=
+      {{ 
+        opacity: 0,
+        /*slide up*/  /*slide down: y:'-50vh'*/       
+        y: '50vh',
+      }}
+      animate=
+      {{ 
+        opacity: 1,        
+        y:'0vh',
+      }}>
+      <FullPage controls={CustomControls}>
+        <Slide {...style}>
+          <Navbar />
+          <Section>
+            <Hero />
+          </Section>
+        </Slide>
 
-            <Slide>
-            <Section>
-                <Services />
-              </Section>
-            </Slide> 
+        <Slide>
+          <Section>
+            <Services />
+          </Section>
+        </Slide>
 
-            <Slide>
-              <Section>
-                <StudyCases />
-              </Section>          
-            </Slide>
-            
-            <Slide>
-              <Section>
-                <Pricing />
-              </Section>          
-            </Slide>
+        <Slide>
+          <Section>
+            <StudyCases />
+          </Section>
+        </Slide>
 
-            <Slide>
-              <Section>
-                <Selection />
-              </Section>          
-            </Slide>
+        <Slide>
+          <Section>
+            <Pricing />
+          </Section>
+        </Slide>
 
-            <Slide>
-              <section className= "w-full h-screen bg-primary mx-auto px-10">
-                <Footer />
-              </section>                  
-            </Slide>
-                
-          </FullPage>
-      </motion.div> 
+        <Slide>
+          <Section>
+            <Selection />
+          </Section>
+        </Slide>
+
+        <Slide>
+          <section className="w-full h-screen bg-primary mx-auto px-10">
+            <Footer />
+          </section>
+        </Slide>
+
+      </FullPage>
+    </motion.div>
   )
 }
