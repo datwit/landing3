2344612ab7b63bc49003ebebb777a83f618e6bb7
Enma Lidia Muñoz Garcia <enmalidia88@gmail.com>
2021-04-14 21:28:47 -0500

    style: :lipstick: Alingning contact icons on the footer
    #53

--- components/Footer/style.js
+++ components/Footer/style.js
@@ -14,7 +14,7 @@ export const FooterLinksWrapper = styled.div.attrs({
     className: "flex flex-wrap overflow-hidden lg:-mx-2"
 })``;
 export const ColumnContent = styled.div.attrs({
-    className: "sm:w-2/4 lg:w-1/4 overflow-hidden lg:my-2 lg:px-2"
+    className: "sm:w-2/4 lg:w-1/4 overflow-hidden lg:my-2 lg:px-2 sm:mb-4"
 })``;
 export const ColumnTitle = styled.h2.attrs({
     className: "text-white font-bold flex justify-start text-lg mb-5"
@@ -33,7 +33,7 @@ svg{
 }
 `;
 export const ContactWrapper = styled.span.attrs({
-    className: "flex items-center"
+    className: "flex"
 })``;
 export const UpperH3 = styled.h3.attrs({
     className: "text-white text-left mb-5 cursor-pointer"
