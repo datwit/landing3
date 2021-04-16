79e0afd3802f6c58b1dc387a6e422bee6d71d6e0
enmalidia <enmalidia88@gmail.com>
2021-04-16 11:40:26 -0400

    style: :iphone: Study cases section: Re-styling responsive components
    #54

--- components/Home/StudyCases/index.js
+++ components/Home/StudyCases/index.js
@@ -3,6 +3,7 @@ import {SCardWrapper, SCardBorder, CardContent, CardTitle, CardSubtitle, ReadMor
 import {ContentWrapper, SectionSubheader, SectionHeader} from '../../../styles/global';
 import { CardSummary, BlogCardBorder, RespBlock, BlogTitle2, DateWrapper} from '../../../components/Blog/style'
 import Link from "next/link"
+import uuid from 'react-uuid'
 
 const StudyCases = () => {
     
@@ -13,19 +14,21 @@ const StudyCases = () => {
         <div className="hidden md:block">
             <ContentWrapper>    
                {
-                    datas.map((data, key) => (
-                        <SCardWrapper key={key}>                
+                    datas.map(data => (
+                        <SCardWrapper key={uuid()}>                
                             <SCardBorder>
-                                <img className="lg:h-48 md:h-36 w-full object-cover object-center" src={data.src} alt={data.alt} />                                
+                                <img className="lg:h-48 md:h-36 w-full object-cover object-center" src={data.img} alt="" />                                
                                 <CardContent>                                                      
                                     <CardTitle>{data.title}</CardTitle>
-                                    <CardSubtitle>{data.description}</CardSubtitle>                                    
-                                        <ReadMore href="" >Learn More
-                                            <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                                <path d="M5 12h14"></path>
-                                                <path d="M12 5l7 7-7 7"></path>
-                                            </svg>
-                                        </ReadMore>                                    
+                                    <CardSubtitle>{data.summary}</CardSubtitle>                                    
+                                        <Link href={`/studycases/${data.id}`}>
+                                            <ReadMore>Learn More
+                                                <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                                    <path d="M5 12h14"></path>
+                                                    <path d="M12 5l7 7-7 7"></path>
+                                                </svg>
+                                            </ReadMore>
+                                        </Link>                                    
                                 </CardContent>    
                             </SCardBorder>
                         </SCardWrapper> 
@@ -35,25 +38,23 @@ const StudyCases = () => {
         </div> 
         <div className="md:hidden">                                      
          {/**********responsive************** */}
-         <ul>
-            {
-                datas.map((data, key1) => (
-                    <li className="" key={key1}>                        
-                        <div className="mx-4 md:mx-0 mb-3">
+            <ul className="-top-2/4 transform translate-y-2/4">
+                {
+                    datas.map(data => (
+                        <BlogCardBorder key={uuid()} className="mb-3">
                             <div className="flex">
-                                <img src={data.src}  className="sm:w-1/5 w-1/2"  alt={data.alt}/>
-                                <RespBlock>                                  
-                                    <Link href={`/studycases/${data.id}`}><BlogTitle2>{data.title}</BlogTitle2></Link>                                      
-                                    
-                                    <p className="">{data.summary}</p>
-                                </RespBlock>  
-                            </div>                        
-                        </div>
-                    </li>
-                )) 
-            }
-         </ul>
-           
+                            <img src={data.img} className="sm:w-1/4 w-1/2" alt="" />
+                            <RespBlock>
+                                <Link href={`/studycases/${data.id}`}>
+                                    <BlogTitle2>{data.title}</BlogTitle2>
+                                </Link>                            
+                                <p>{data.author}</p>
+                            </RespBlock>
+                            </div>
+                        </BlogCardBorder>
+                    )) 
+                }
+            </ul>           
         </div>  
     </div>  
         
