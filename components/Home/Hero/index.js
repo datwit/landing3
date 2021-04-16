9062e0b4bac8f429d8fd711294ad987c91dc6fc4
enmalidia <enmalidia88@gmail.com>
2021-04-16 15:13:45 -0400

    style: :lipstick: Hero section: centering elements. Testing subheader text
    #51

--- components/Home/Hero/index.js
+++ components/Home/Hero/index.js
@@ -4,18 +4,19 @@ import {MainHeaderCont} from './style';
 
 
 const Hero = ()=>(         
-      
-      <MainHeaderCont>
-          <h1>Bring your idea,
-            <br/>
-            we will make it real!
-          </h1>
-          <h4>
-            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem
-            <br/>
-            cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
-          </h4>
-          <CTA buttonName={'Contact us'}/>
-      </MainHeaderCont>     
+        <div className="relative top-2/4 transform -translate-y-2/4">
+          <MainHeaderCont>
+              <h1>Bring your idea,
+                <br/>
+                we will make it real!
+              </h1>
+              <h4>
+                ...the sky isn't the limit for us...just the start
+                {/* <br/>
+                cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua. */}
+              </h4>
+              <CTA buttonName={'Contact us'}/>
+          </MainHeaderCont> 
+        </div>          
 )
 export default Hero
