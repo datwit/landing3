5235add44c58fad52686e6a09fc406d43423ec51
enmalidia <enmalidia88@gmail.com>
2021-04-16 14:48:03 -0400

    style: :iphone: Aligning elements on the footer for the xs and sm sizes
    #54

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import {Fragment} from 'react'
-import {FooterContainer, SocialList, FooterHeader, FooterLinksWrapper, ColumnContent, ColumnTitle, SocialListItem, ContactWrapper, UpperH3, DownH3, CopyRight} from './style'
+import {Divider, FooterContainer, SocialList, FooterHeader, FooterLinksWrapper, ColumnContent, ColumnTitle, SocialListItem, ContactWrapper, UpperH3, DownH3, CopyRight} from './style'
 import datas from './data.json'
 import {FiGithub, FiFacebook, FiTwitter, FiLinkedin} from 'react-icons/fi'
 
@@ -20,7 +20,8 @@ const Footer = () => {
                             <Link  href={datas[2].url}><SocialListItem><FiGithub /></SocialListItem></Link>
                             <Link  href={datas[3].url}><SocialListItem><FiTwitter /></SocialListItem></Link>                           
                         </SocialList>
-                    </ColumnContent>                
+                        <Divider />  
+                    </ColumnContent>                                  
                     <ColumnContent>
                         <ColumnTitle>CONTACT US</ColumnTitle>
                         <ContactWrapper>
@@ -33,18 +34,18 @@ const Footer = () => {
                             <svg className="h-7 w-7 text-white"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round">  <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z" /></svg>
                             <DownH3>+58 658 987</DownH3>
                         </ContactWrapper>
-                    </ColumnContent>                
+                        <Divider />
+                    </ColumnContent>                                  
                     <ColumnContent>
                         <ColumnTitle>SPECIALIZED CONTENT</ColumnTitle>                        
                         <Link href='/partner'><UpperH3>Partner area</UpperH3></Link>
-                        <Link href='/team'><DownH3>Team member area</DownH3></Link>                        
+                        <Link href='/team'><DownH3>Team member area</DownH3></Link>
+                    <Divider />                          
                     </ColumnContent>                
                     <ColumnContent>
                         <ColumnTitle>DATA PROTECTION</ColumnTitle>
-                        <Link href='/info/policy'><h3 className="text-white text-left mb-5 cursor-pointer">Privacy Policy</h3></Link>
-                        <Link href='/info/terms'><h3 className="text-white text-left cursor-pointer" >Terms and Conditions</h3></Link>
-                        {/*<UpperH3>Privacy Policy</UpperH3>
-                        <DownH3>Terms and Conditions</DownH3>*/}
+                        <Link href='/info/policy'><UpperH3>Privacy Policy</UpperH3></Link>
+                        <Link href='/info/terms'><DownH3>Terms and Conditions</DownH3></Link>                     
                     </ColumnContent>
                 </FooterLinksWrapper>                    
             </FooterContainer>                
