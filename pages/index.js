89b52b6fe6621cb2c3d64962aeb749a872bc6189
Enma Lidia Muñoz Garcia <enmalidia88@gmail.com>
2021-04-18 19:41:01 -0500

    style: :lipstick: Adding easing to the bar color changing
    #53

--- pages/index.js
+++ pages/index.js
@@ -7,55 +7,72 @@ import Services from '../components/Home/Services'
 import Pricing from '../components/Home/Pricing'
 import Footer from '../components/Footer'
 import Selection from '../components/Selection'
-import Navbar from '../components/Navbar'
+import Navbar from '../components/Navbar' 
 import { motion } from 'framer-motion'
 
 export default function Home() {
-  const style = {
-    height: 'calc(100% - 80px)',
+  const style={
+    height:'calc(100% - 80px)',    
+  }  
+
+  const before = (event) => {
+    const navb = document.getElementById('navbar')
+    if(event.from === 5){
+      navb.classList.replace('bar-vanished','bg-white')
+    }
   }
+  const after = (event) => {
+    const navb = document.getElementById('navbar')
+    if(event.to === 5 ){
+      navb.classList.replace('bg-white','bar-vanished')
+    }
+  }  
   
   return (
     <motion.div initial={{opacity:0,  y: 200}} animate={{opacity:1, y:0}}> 
-        <FullPage controls={CustomControls}>
-          <Slide {...style}>
-            <Navbar />
-            <Section>
-              <Hero />
-            </Section>
-          </Slide>
+            <FullPage controls={CustomControls} beforeChange={before} afterChange={after}> 
 
-          <Slide>
-            <Section>
-              <Services />
-            </Section>
-          </Slide>
+              <Slide {...style}>
+                <div className="flex items-center flex-wrap h-20 w-full fixed top-0 bg-fixed bg-white z-10" id='navbar'>              
+                  <Navbar/> 
+                </div>       
+                <Section>                  
+                    <Hero />                  
+                </Section>        
+              </Slide>
 
-          <Slide>
-            <Section>
-              <StudyCases />
-            </Section>
-          </Slide>
+              <Slide>
+              <Section>
+                  <Services />
+                </Section>
+              </Slide> 
 
-          <Slide>
-            <Section>
-              <Pricing />
-            </Section>
-          </Slide>
+              <Slide>
+                <Section>
+                  <StudyCases />
+                </Section>          
+              </Slide>
+              
+              <Slide>
+                <Section>
+                  <Pricing />
+                </Section>          
+              </Slide>
 
-          <Slide>
-            <Section>
-              <Selection />
-            </Section>
-          </Slide>
+              <Slide>
+                <Section>
+                  <Selection />
+                </Section>          
+              </Slide>
 
-          <Slide>
-            <section className="w-full h-screen bg-primary mx-auto px-10">
-              <Footer />
-            </section>
-          </Slide>
-
-        </FullPage>
-    </motion.div>
+              <Slide>
+                <section className= "w-full h-screen bg-primary mx-auto px-10">
+                  <Footer />
+                </section>                 
+              </Slide>
+              
+          </FullPage>
+      
+      </motion.div>       
   )
 }
