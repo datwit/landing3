f167f901f463ca507e07d1488a7d414c1d799eba
enmalidia <enmalidia88@gmail.com>
2021-04-28 14:53:13 -0400

    feat: :construction: Services section: Coding card reveal(web version)
    #54

--- components/Home/Services/index.js
+++ components/Home/Services/index.js
@@ -24,7 +24,93 @@ const Services = () => {
         <div className="container px-5 mx-auto">
             <SectionSubheader>Lorem ipsum dolor sit amet consectetur adipisicing elit.</SectionSubheader>
             <ContentWrapper className="relative maxHeight">
-                <IconWrapper className={openTab === 2 || openTab === 1 ? "hidden" : "block" }>                                   
+                <div className="hidden md:block">
+                    <div className="w-full inline-flex">                                                
+                        <div className="sm:w-2/4 flex flex-col border border-gray-300 shadow hover:shadow-lg transition duration-200 ease-in">                           
+                            <SoftDSVG/>                           
+                            <IconTitle onClick={e => 
+                                { 
+                                e.preventDefault();
+                                setOpenTab(1);
+                                }}
+                                data-toggle="tab"
+                                href="#link1"
+                                role="tablist">
+                                    Software Development
+                            </IconTitle>
+                            <div className={openTab === 1 ? "open" : "hidden"} id="link1" >                             
+                                <a className="flex justify-end" href="#" onClick={e =>
+                                {
+                                e.preventDefault();
+                                setOpenTab(0);
+                                }} >
+                                <svg className="h-8 w-8 text-primary"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round">  <line x1="18" y1="6" x2="6" y2="18" />  <line x1="6" y1="6" x2="18" y2="18" /></svg>
+                                </a>
+                                {
+                                    datas.soft.map((data, key) => (
+                                        <div className="ml-2" key={key}>
+                                            <BulletTitle>{data.title}</BulletTitle>
+                                            <BulletSubtitle>{data.subtitle}</BulletSubtitle>
+                                        {
+                                            data.items.map((data,key1)=>(
+                                            <div  key={key1}> 
+                                                <ServiceBulletsOuter>
+                                                    <ServiceBulletsSpan>
+                                                    <svg className="h-8 w-8 text-secondary2"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round">  <circle cx="12" cy="12" r="10" />  <polyline points="12 16 16 12 12 8" />  <line x1="8" y1="12" x2="16" y2="12" /></svg>
+                                                    </ServiceBulletsSpan>{data.tit}
+                                                </ServiceBulletsOuter>
+                                            </div>
+                                            ))
+                                        }
+                                        </div>
+                                    ))
+                                }                         
+                            </div>
+                        </div>                        
+                        <div className="sm:w-2/4 flex flex-col border border-gray-300 shadow hover:shadow-lg transition duration-200 ease-in">                                                                               
+                            <DataScSVG />                            
+                            <IconTitle onClick={e => 
+                                {
+                                e.preventDefault();
+                                setOpenTab(2);
+                                }}
+                                data-toggle="tab"
+                                href="#link2"
+                                role="tablist">
+                                    Data Science
+                            </IconTitle>
+                            <div className={openTab === 2 ? "open" : "hidden"} id="link2">                   
+                                <a className="flex justify-end" href="#" onClick={e =>
+                                {
+                                e.preventDefault();
+                                setOpenTab(0);
+                                }} >
+                                <svg className="h-8 w-8 text-primary"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round">  <line x1="18" y1="6" x2="6" y2="18" />  <line x1="6" y1="6" x2="18" y2="18" /></svg>
+                                </a>
+                                {
+                                    datas.data.map((data, key) => (
+                                        <div  key={key}>
+                                        <BulletTitle>{data.title}</BulletTitle>
+                                        <BulletSubtitle>{data.subtitle}</BulletSubtitle>
+                                        {
+                                            data.items.map((data,key1)=>(
+                                            <div  key={key1}>
+                                                <ServiceBulletsOuter>
+                                                    <ServiceBulletsSpan>
+                                                    <svg className="h-8 w-8 text-secondary2"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round">  <circle cx="12" cy="12" r="10" />  <polyline points="12 16 16 12 12 8" />  <line x1="8" y1="12" x2="16" y2="12" /></svg>
+                                                    </ServiceBulletsSpan>{data.tit}
+                                                </ServiceBulletsOuter>
+                                            </div>  
+                                            ))
+                                        }
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* <IconWrapper className={openTab === 2 || openTab === 1 ? "hidden" : "block" }>                                   
                         <div className="sm:w-2/4 flex flex-col">                           
                                 <SoftDSVG/>                           
                         <IconTitle onClick={e => 
@@ -145,7 +231,7 @@ const Services = () => {
                      </div>
                     
                                     
-                </TabContent>
+                </TabContent> */}
             </ContentWrapper>
         </div>       
         
