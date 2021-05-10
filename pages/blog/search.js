9e55546349adff4346ed606fe4d7a8d5a104cfc6
alcantara2575 <alcantara2575@gmail.com>
2021-05-10 09:04:02 -0400

    feat: :sparkles: Add paginate to search results with slide
    #61

--- pages/blog/search.js
+++ pages/blog/search.js
@@ -11,6 +11,12 @@ import Navbar from '../../components/Navbar'
 import { motion } from 'framer-motion'
 import Head from 'next/head';
 
+import ReactPaginate from 'react-paginate'
+import { useEffect, useState } from 'react'
+import { BlogTitle1, PaginationWrapper } from '../../components/Blog/style'
+
+
+
 import posts from '../../cache/posts.json'
     posts.map(post => (
       post.Stringtags='',
@@ -25,8 +31,13 @@ const BlogResults = () => {
     height:'calc(100% - 80px)',    
   }  
 
-  const router = useRouter()
 
+  const [offset, setOffset] = useState(0);
+  const [data, setData] = useState([]);
+  const [ perPage, setPerPage ] = useState(10);
+  const [pageCount, setPageCount] = useState(0)
+
+  const router = useRouter()
   const cat = router.query.p  
   const que = router.query.q
   const results = cat != null ? 
@@ -38,6 +49,51 @@ const BlogResults = () => {
     | post.Stringtags.toLowerCase().includes(que)
       )
     )
+
+  //getting our data 
+  const getData = () => {
+    const data = results
+    const cant = window.innerHeight <= 640 && window.innerWidth <= window.innerHeight ? 
+       4 : window.innerHeight >= 800 && window.innerWidth <= 765 ? 4 : 3
+    
+    setPerPage(cant)
+    //slicing data   
+    const slice = data.slice(offset, offset + cant)
+    const postData = slice.map((item, key6) =>
+      <div key={key6}>
+        <div className="mx-4 md:mx-0 mb-3">
+            <div className="flex">
+              <Link href={`/blog/${item.id}`}><img className="sm:w-1/6 w-1/2" src={item.img} alt="" /></Link>         
+              <RespBlock>
+              <DateWrapper>
+                  {format(parseISO(item.date), 'MMMM do, uuu')}
+              </DateWrapper>
+                <Link href={`/blog/${item.id}`}><BlogTitle1>{item.title}</BlogTitle1></Link>
+                <CardSummary className="hidden sm:block">{item.summary}</CardSummary>
+              </RespBlock>
+          </div>
+        </div>
+      </div>
+    )
+    setData(postData)
+    setPageCount(Math.ceil(data.length / cant))
+  }
+
+  //callingData
+  useEffect(() => {
+    getData()
+  }, [offset])
+
+  //clicking from page to page
+  const handlePageClick = (e) => {
+    const selectedPage = e.selected;
+    setOffset(selectedPage * perPage)
+  };
+
+  
+  const nextSVG = <svg className="h-8 w-8 text-secondary2" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">  <polyline points="13 17 18 12 13 7" />  <polyline points="6 17 11 12 6 7" /></svg>
+  const prevSVG = <svg className="h-8 w-8 text-secondary2" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">  <polyline points="11 17 6 12 11 7" />  <polyline points="18 17 13 12 18 7" /></svg>
+  
   
 return (
   <>
@@ -49,39 +105,35 @@ return (
         <FullPage controls={CustomControls}>
           <Slide {...style}>
             <Section>
-            <div className="container px-5 mx-auto">        
-              <SectionHeader>Search Results</SectionHeader>
-              <ContentWrapper>            
-              { 
-                results.length > 0         
-                ? <div>
-                      <ul>
-                        {
-                          results.map(({title, summary, date, img, id },key1) => (
-                          <li key={key1}>                        
-                            <div className="mx-4 md:mx-0 mb-3">
-                              <div className="flex">
-                                  <img src={img}  className="sm:w-1/5 w-1/2"  alt=""/>
-                                    <RespBlock>                                  
-                                      <Link href={`/blog/${id}`}><BlogTitle2>{title}</BlogTitle2></Link>                                                  
-                                      <DateWrapper>
-                                          {format(parseISO(date), 'MMMM do, uuu')}
-                                      </DateWrapper>
-                                      <CardSummary className="hidden sm:block">{summary}</CardSummary>
-                                    </RespBlock>  
-                              </div>                        
-                            </div>
-                          </li>
-                        ))
-                        }
-                      </ul>
-                  </div>
-                : <div className="mx-auto">
-                  <p>No results found 😢 </p>
+            <div className="container px-5 mx-auto relative transform mt-20">
+            <SectionHeader>Search Results</SectionHeader>
+              {/*pagination*/}
+                <div className="flex flex-wrap">
+                  {data}
+                  { results ==0 ?
+                      <div className="mx-auto">
+                      <p>No results found 😢 </p>
+                      </div>
+                      :
+                      pageCount >= 2 ?
+                      <PaginationWrapper>
+                        <ReactPaginate
+                          previousLabel={prevSVG}
+                          nextLabel={nextSVG}
+                          breakLabel={"..."}
+                          breakClassName={"break-me"}
+                          pageCount={pageCount}
+                          onPageChange={handlePageClick}
+                          containerClassName={"pagination"}
+                          subContainerClassName={"pages pagination"}
+                          activeClassName={"active"}
+                        />
+                      </PaginationWrapper>
+                      :
+                      []
+                  }
                 </div>
-              }
-                </ContentWrapper>           
-              </div>               
+              </div>            
             </Section>        
           </Slide>       
 
