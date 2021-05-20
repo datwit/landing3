2ba6be91f2d12c61aca361c1b395dda965bd1f45
enmalidia <enmalidia88@gmail.com>
2021-05-20 10:46:28 -0400

    feat: :sparkles: Adding svg illustration to the About Datwit section. Adding animation
    Creating the illustration. Adding colors to match gray scale used before on services. Adjusting svg to make it responsible .
    
    #62

--- components/AboutUs/Datwit/style.js
+++ components/AboutUs/Datwit/style.js
@@ -5,7 +5,25 @@ export const DatwitWrapper = styled.div.attrs({
 })``;
 export const ImgWrapper = styled.div.attrs({
     className: "lg:w-1/2 w-full"
-})``;
+})
+`
+    svg {
+        width:  auto;
+        height: auto;
+    }
+    #sign, #hand2, #hand1{
+        animation: sign-anime 1s linear infinite alternate; 
+        -webkit-animation: sign-anime 1s linear infinite alternate;
+    }
+    @keyframes sign-anime {
+        0%{
+            transform: translateY(0);
+        }
+        100%{
+            transform: translateY(10px);
+        }
+    }
+`;
 export const TextBlock = styled.div.attrs({
     className: "lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0"
 })``;
