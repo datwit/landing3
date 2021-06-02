bf92f7bc28f20039325b848795d98ec4d1c9c11b
enmalidia <enmalidia88@gmail.com>
2021-06-02 11:50:31 -0400

    feat: :sparkles: Home page-study cases: Adjusting visual components on mobile
    #64

--- components/Home/StudyCases/StudyCasesCard.js
+++ components/Home/StudyCases/StudyCasesCard.js
@@ -5,18 +5,18 @@ import Link from "next/link"
 const StudyCasesCard = ({img, title,summary,id}) => {
     return (
         <SCardBorder>
-            <img className="lg:h-48 md:h-36 w-full object-cover object-center" src={img} alt="" />                                
+            <img className="lg:h-48 h-36 w-full object-cover object-center" src={img} alt="" />                                
             <CardContent>                                                      
                 <CardTitle>{title}</CardTitle>
                 <CardSubtitle>{summary}</CardSubtitle>                                    
-                    <Link href={`/studycases/${id}`}>
-                        <ReadMore>Learn More
-                            <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                <path d="M5 12h14"></path>
-                                <path d="M12 5l7 7-7 7"></path>
-                            </svg>
-                        </ReadMore>
-                    </Link>                                    
+                <Link href={`/studycases/${id}`}>
+                    <ReadMore>Learn More
+                        <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                            <path d="M5 12h14"></path>
+                            <path d="M12 5l7 7-7 7"></path>
+                        </svg>
+                    </ReadMore>
+                </Link>                                    
             </CardContent>    
         </SCardBorder>
 )
