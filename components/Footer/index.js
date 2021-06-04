a30ae8f41cce4170fd0224a0b93f6680ae655f3f
enmalidia <enmalidia88@gmail.com>
2021-06-04 14:17:51 -0400

    feat: :sparkles: Testing new visual components on the footer mobile sizes #64

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,8 +1,10 @@
 import Link from 'next/link'
 import {Fragment} from 'react'
-import {Divider, FooterContainer, SocialList, FooterHeader, FooterLinksWrapper, ColumnContent, ColumnTitle, SocialListItem, ContactWrapper, UpperH3, DownH3, CopyRight} from './style'
+import {Divider, FooterAccordionList, FooterContainer, SocialList, FooterHeader, FooterLinksWrapper, ColumnContent, ColumnTitle, SocialListItem, ContactWrapper, UpperH3, DownH3, CopyRight, FooterAccordionWrapper} from './style'
 import datas from './data.json'
+import links from './pageslink.json';
 import {FiGithub, FiFacebook, FiTwitter, FiLinkedin} from 'react-icons/fi'
+import FooterAccItem from './FooterAccItem';
 
 const Footer = () => {
     const year = new Date().getFullYear()
@@ -10,44 +12,73 @@ const Footer = () => {
         <Fragment>            
             <FooterContainer>                                    
                    {/*  <img className="mx-auto mb-6" src="/images/logo-grande.png" alt="logo" width="350px" /> */}
-                    <FooterHeader>Let's work together</FooterHeader>                
-                <FooterLinksWrapper>        
-                    <ColumnContent>
-                        <ColumnTitle>FOLLOW US</ColumnTitle>
-                        <SocialList>
-                            <Link href={datas[0].url}><SocialListItem><FiLinkedin /></SocialListItem></Link>
-                            <Link  href={datas[1].url}><SocialListItem><FiFacebook /></SocialListItem></Link>
-                            <Link  href={datas[2].url}><SocialListItem><FiGithub /></SocialListItem></Link>
-                            <Link  href={datas[3].url}><SocialListItem><FiTwitter /></SocialListItem></Link>                           
-                        </SocialList>
-                        <Divider />  
-                    </ColumnContent>                                  
-                    <ColumnContent>
-                        <ColumnTitle>CONTACT US</ColumnTitle>
-                        <ContactWrapper>
-                            <svg className="h-7 w-7 text-white"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round">  <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />  <polyline points="22,6 12,13 2,6" /></svg>
-                            <a href="mailto:contact@datwit.com">
-                            <UpperH3>contact@datwit.com</UpperH3>
-                            </a>
-                        </ContactWrapper>
-                        <ContactWrapper>
-                            <svg className="h-7 w-7 text-white"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round">  <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z" /></svg>
-                            <DownH3>+58 658 987</DownH3>
-                        </ContactWrapper>
-                        <Divider />
-                    </ColumnContent>                                  
-                    <ColumnContent>
-                        <ColumnTitle>SPECIALIZED CONTENT</ColumnTitle>                        
-                        <Link href='/partner'><UpperH3>Partner area</UpperH3></Link>
-                        <Link href='/team'><DownH3>Team member area</DownH3></Link>
-                    <Divider />                          
-                    </ColumnContent>                
-                    <ColumnContent>
-                        <ColumnTitle>DATA PROTECTION</ColumnTitle>
-                        <Link href='/info/policy'><UpperH3>Privacy Policy</UpperH3></Link>
-                        <Link href='/info/terms'><DownH3>Terms and Conditions</DownH3></Link>                     
-                    </ColumnContent>
-                </FooterLinksWrapper>                    
+                    <FooterHeader>Let's work together</FooterHeader> 
+                <div className="hidden sm:block">               
+                    <FooterLinksWrapper>                    
+                        <ColumnContent>
+                            <ColumnTitle>FOLLOW US</ColumnTitle>
+                            <SocialList>
+                                <Link href={datas[0].url}><SocialListItem><FiLinkedin /></SocialListItem></Link>
+                                <Link  href={datas[1].url}><SocialListItem><FiFacebook /></SocialListItem></Link>
+                                <Link  href={datas[2].url}><SocialListItem><FiGithub /></SocialListItem></Link>
+                                <Link  href={datas[3].url}><SocialListItem><FiTwitter /></SocialListItem></Link>                           
+                            </SocialList>                            
+                        </ColumnContent>                                  
+                        <ColumnContent>
+                            <ColumnTitle>CONTACT US</ColumnTitle>
+                            <ContactWrapper>
+                                <svg className="h-7 w-7 text-white"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round">  <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />  <polyline points="22,6 12,13 2,6" /></svg>
+                                <a href="mailto:contact@datwit.com">
+                                <UpperH3>contact@datwit.com</UpperH3>
+                                </a>
+                            </ContactWrapper>
+                            <ContactWrapper>
+                                <svg className="h-7 w-7 text-white"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round">  <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z" /></svg>
+                                <DownH3>+58 658 987</DownH3>
+                            </ContactWrapper>                            
+                        </ColumnContent>                                  
+                        <ColumnContent>
+                            <ColumnTitle>SPECIALIZED CONTENT</ColumnTitle>                        
+                            <Link href='/partner'><UpperH3>Partner area</UpperH3></Link>
+                            <Link href='/team'><DownH3>Team member area</DownH3></Link>
+                        <Divider className="sm:hidden" />                          
+                        </ColumnContent>                
+                        <ColumnContent>
+                            <ColumnTitle>DATA PROTECTION</ColumnTitle>
+                            <Link href='/info/policy'><UpperH3>Privacy Policy</UpperH3></Link>
+                            <Link href='/info/terms'><DownH3>Terms and Conditions</DownH3></Link>                     
+                        </ColumnContent>                                      
+                    </FooterLinksWrapper>  
+                </div> 
+                <div className="sm:hidden">
+                    <FooterLinksWrapper>                    
+                        <ColumnContent>
+                            <ColumnTitle>CONTACT US</ColumnTitle>
+                            <SocialList>
+                                <Link href={datas[0].url}><SocialListItem><FiLinkedin /></SocialListItem></Link>
+                                <Link  href={datas[1].url}><SocialListItem><FiFacebook /></SocialListItem></Link>
+                                <Link  href={datas[2].url}><SocialListItem><FiGithub /></SocialListItem></Link>
+                                <Link  href={datas[3].url}><SocialListItem><FiTwitter /></SocialListItem></Link> 
+                                <Link href="mailto:contact@datwit.com"><svg className="h-7 w-7 text-white mr-2"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round">  <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />  <polyline points="22,6 12,13 2,6" /></svg></Link> 
+                                <Link href="tel:+58 658 987"><svg className="h-7 w-7 text-white mr-2"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round">  <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z" /></svg></Link>                       
+                            </SocialList>
+                            {/* <Divider />   */}
+                        </ColumnContent>                        
+                        <FooterAccordionWrapper>
+                            <FooterAccordionList>
+                            {
+                                links.map((lik, key)=> {
+                                return (
+                                <li {...{ className: 'accordion-list__item', key }}>
+                                    <FooterAccItem {...lik} />
+                                </li>
+                                )
+                            })
+                            }
+                            </FooterAccordionList>
+                        </FooterAccordionWrapper>                 
+                    </FooterLinksWrapper>
+                </div>                   
             </FooterContainer>                
             <CopyRight>&copy; {year} DatWit</CopyRight>
         </Fragment>
--- components/Footer/style.js
+++ components/Footer/style.js
@@ -7,13 +7,13 @@ export const FooterHeader = styled.h1.attrs({
     className: "text-white text-center text-4xl my-2"
 })``;
 export const FooterLinksWrapper = styled.div.attrs({
-    className: "flex flex-wrap overflow-hidden lg:-mx-2 justify-center sm:justify-start md:px-8"
+    className: "sm:flex sm:flex-wrap overflow-hidden lg:-mx-2 justify-center sm:justify-start md:px-8"
 })``;
 export const ColumnContent = styled.div.attrs({
     className: "sm:w-2/4 lg:w-1/4 overflow-hidden lg:my-2 lg:px-2 mb-4"
 })``;
 export const ColumnTitle = styled.h2.attrs({
-    className: "text-white font-bold text-lg sm:mb-5"
+    className: "text-white font-bold text-lg my-2 sm:my-0 sm:mb-5"
 })
 `
 @media (max-width: 639px){
@@ -25,7 +25,14 @@ export const ColumnTitle = styled.h2.attrs({
 `;
 export const SocialList = styled.ul.attrs({
     className: "inline-flex"
-})``;
+})
+`  
+@media (max-width: 640px){
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+`;
 export const SocialListItem = styled.li.attrs({
     className: "mr-2 h-8 w-8 cursor-pointer"
 })
@@ -78,3 +85,79 @@ export const Divider = styled.div.attrs({
 // background: #b7b7bc;
 `;
 
+export const FooterAccordionItem = styled.div
+`
+// padding-bottom:10px; 
+border-bottom: 1px solid #fcfcfc;    
+`;
+export const FooterAccordionWrapper = styled.div.attrs({
+    className: ""
+})
+`
+/* padding-bottom: 30px; */
+`;
+export const FooterAccordionList = styled.div
+`
+    list-style: none;
+    margin: 0;
+    padding: 0;       
+    overflow: hidden;        
+`;
+export const FooterAccordionTitle = styled.h3.attrs({
+    className: "text-lg text-white mx-auto"
+})
+`
+    text-transform: uppercase;
+    &:hover{
+        color: #00baff;
+    }
+`;
+export const FooterTitleWrapper = styled.div.attrs({
+    className: ""
+})
+`
+    /* display: block; */
+    padding: 0.5rem 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: transparent;
+    z-index: 2;
+    position: relative;
+    cursor:pointer;
+
+    .icon-rotate {       
+        transform: rotate(180deg); 
+                    
+    }   
+`
+export const FooterAccItemInner = styled.div
+`
+    max-height: 0;
+    overflow: hidden;
+    text-transform: cubic-bezier(0.95, 0.05, 0.795, 0.035);
+    transition-duration: 0.5s;
+    transition-property: max-height;    
+    position: relative;
+    background:transparent;  
+    // border-left: 3px solid #e69c24;     
+        
+`;
+export const FooterAccItemLinks = styled.h3.attrs({
+    className: "text-base text-white font-light"
+})
+`
+    text-align: center;
+    
+`;
+export const FooterAccordionIcon= styled.svg
+`
+    width: 1.2rem;
+    height: 1.2rem;
+    transition: transform 0.3s ease-in-out;
+    background-size: contain;
+    background-repeat: no-repeat;  
+    opacity: 0.6;
+    color:white;
+
+`;
