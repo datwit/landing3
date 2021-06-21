a1d0f5f1d1f69d9ec7b5b1515f24a00a8f85a341
Enma Lidia Muñoz Garcia <enmalidia88@gmail.com>
2021-06-20 22:37:45 -0500

    feat: :sparkles: Partner page: Adding Illiustrations. Adjusting code
    #77

--- components/Partner/Find/index.js
+++ components/Partner/Find/index.js
@@ -1,8 +1,12 @@
 import {ContentWrapper, SectionHeader, SectionSubheader} from '../../../styles/global'
 import {IconWrapper} from '../../Team/Benefits/style'
+import { IconBlock} from './style'
 import datas from './data.json'
 import FindBlock from './FindBlock'
 import uuid from 'react-uuid'
+import { CustomSVG } from './CustomSVG'
+import {IntegrationSVG} from './IntegrationSVG'
+import {AgileSVG} from './AgileSVG'
 
 const Find = ({classes}) => {
     return (
@@ -10,14 +14,27 @@ const Find = ({classes}) => {
             <SectionHeader>Why you should hire us</SectionHeader>
             <SectionSubheader>We can work together to build the product you have in mind through intuitive designs and great tools to back them up.</SectionSubheader>
             <h3 className="text-gray-600">Our experience creating IT solutions includes:</h3>   
-                <ContentWrapper>                    
-                    {
-                        datas.map(data => (
-                            <IconWrapper key={uuid()}>                       
-                                <FindBlock  src={data.src} title={data.title} description={data.description} />                                             
-                            </IconWrapper>
-                        ))
-                    }                                   
+                <ContentWrapper>                       
+                    <IconWrapper>                      
+                        <IconBlock>
+                            <CustomSVG />
+                            <FindBlock title={datas[0].title} description={datas[0].description}/>
+                        </IconBlock>                                               
+                    </IconWrapper>
+                    <IconWrapper>                      
+                        <IconBlock>                           
+                            <IntegrationSVG />
+                            <FindBlock title={datas[1].title} description={datas[1].description} />
+                        </IconBlock>                                               
+                    </IconWrapper>
+                    <IconWrapper>                      
+                        <IconBlock>                           
+                            <AgileSVG />
+                            <FindBlock title={datas[2].title} description={datas[2].description} />
+                        </IconBlock>                                               
+                    </IconWrapper>
+                      
+                                                      
                 </ContentWrapper>
         </div>
     )
