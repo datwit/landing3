f52286c5c60a17a6ca43c2704945b430525b7997
Enma Lidia Muñoz Garcia <enmalidia88@gmail.com>
2021-07-16 22:42:29 -0400

    feat: :sparkles: Homepage-pricing section: Adding first drafts of the texts
    #106 #108

--- components/Home/Pricing/style.js
+++ components/Home/Pricing/style.js
@@ -4,17 +4,23 @@ export const OuterWrapper = styled.div.attrs({
     className: "p-4 md:w-1/3 w-full"
 })``;
 export const Field = styled.h2.attrs({
-    className: "text-sm tracking-widest title-font mb-1 font-medium pt-2 h-12"
+    className: "text-sm tracking-widest title-font mb-1 font-medium pt-2"
 })``;
 export const Price = styled.h1.attrs({
     className: "text-5xl text-primary mb-1 leading-none mx-auto"
 })``;
 export const LilDescrip = styled.p.attrs({
-    className: "text-xs text-gray-500 mt-3 border-b border-gray-200"
+    className: "text-xs text-gray-500 mt-1 border-b border-gray-200"
 })``;
 export const PBulletsWrapper = styled.p.attrs({
-    className: "flex items-center text-gray-600 mb-2 text-xs sm:text-sm"
-})``;
+    className: "flex items-center text-gray-600 mb-2 mt-2 text-xs"
+})
+`
+@media (min-width: 640px){
+    font-size: 0.850rem;
+    line-height: 1.15rem;
+}
+`;
 export const PBullets = styled.span.attrs({
     className: "w-5 h-5 mr-2 inline-flex items-center justify-center flex-shrink-0"
 })
--- pages/index.js
+++ pages/index.js
@@ -72,7 +72,7 @@ export default function Home() {
 
                 <Slide>
                   <Section classes={'w-full h-screen'}>
-                    <Pricing classes={"container px-5 mx-auto relative top-2/4 transform -translate-y-2/4 mt-4"} />
+                    <Pricing classes={"container px-5 mx-auto relative top-2/4 transform -translate-y-2/4 mt-8"} />
                   </Section>
                 </Slide>
 
