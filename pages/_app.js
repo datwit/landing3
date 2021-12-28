b46aa39470d06a722321b1dd7638f5d641b4ddbe
Enma Lidia Muñoz Garcia <enmalidia88@gmail.com>
2021-12-27 23:08:12 -0500

    feat: :sparkles: Testing GTM container

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,16 @@
 import '../styles/tailwind.css' 
-import Head from 'next/head';
+import Head from 'next/head'
+import TagManager from 'react-gtm-module'
+import React,{useEffect} from 'react'
 
 function MyApp({ Component, pageProps }) {
-  <Head>
-        <link rel="shortcut icon" href="/favicon.ico" />
-  </Head>
-  return <Component {...pageProps} />
+    useEffect(() => {
+        TagManager.initialize({ gtmId: 'GTM-P79KWZ5' });
+    }, []);  
+    <Head>
+            <link rel="shortcut icon" href="/favicon.ico" />
+    </Head>
+    return <Component {...pageProps} />
 }
 
 export default MyApp
