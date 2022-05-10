b7a51004a2e7d8ca7b90ac938be4053402c7d7cb
Yanet Conde <yanet@pop-os.localdomain>
2022-05-10 11:41:17 +0200

    Spelling and content

--- components/AboutUs/Datwit/index.js
+++ components/AboutUs/Datwit/index.js
@@ -13,15 +13,15 @@ const Datwit = ({classes}) => {
                 <TextBlock>
                     <DHeader>Datwit</DHeader>
                     <DParagraph> Datwit was founded in 2019 by a group of friends in Belgrade, Serbia. 
-                        From the very beginning, our main goal has been to create multipurpose software 
-                        solutions by integrating modern techniques like Cloud Computing, Machine Learning, 
+                        From the very beginning, our main goal has been to create AI/ML powered multipurpose smart solutions
+                        by integrating modern techniques like Cloud Computing, Machine Learning, 
                         Data Analysis and micro-services. We integrate our artificial intelligence 
                         solutions into other software applications.
                         <br />
                         At Datwit, we want to help companies make their own small impact on the world
                         around them and expand their possibilities.
                         <br />
-                        Take a look at our open solutions!
+                        Find our open software offerings on Github!
                         <GitLink href="https://github.com/datwit">                        
                             <FiGithub className="h-8 w-8 text-secondary2 ml-4 hover:text-secondary1" />
                         </GitLink>
