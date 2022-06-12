1943ed3220adf692cbc3e3e8c391eeed6fefbd93
alcantara2575 <alcantara2575@gmail.com>
2022-06-12 09:37:30 -0400

    feat: :sparkles: Add cookies consents
    #177

--- pages/_app.js
+++ pages/_app.js
@@ -2,17 +2,32 @@ import '../styles/tailwind.css'
 import Head from 'next/head'
 import TagManager from 'react-gtm-module'
 import React,{useEffect} from 'react'
+import CookieConsent from 'react-cookie-consent';
+import Link from 'next/dist/client/link';
 
 function MyApp({ Component, pageProps }) {
 
-    useEffect(() => {
-      TagManager.initialize({ gtmId: 'GTM-MJQVFNP' });        
-    }, [pageProps]);                     
+    const handleAcceptCookie = () => {
+      TagManager.initialize({ gtmId: 'GTM-MJQVFNP' });
+    }
        
     <Head>
         <link rel="shortcut icon" href="/favicon.ico" />
     </Head>
-    return <Component {...pageProps} />
+    return (
+      <>
+        <CookieConsent
+          onAccept={handleAcceptCookie}
+          debug={false}
+          location="bottom"
+          containerClasses="cookies-consent"
+          >
+          Al confirmar utilizaremos cookies para garantizar que obtengas la mejor experiencia en nuestro sitio web.     
+          <Link href="/info/policy/" className="links-cookies-consent"> Privacy Policy</Link>
+        </CookieConsent>
+        <Component {...pageProps} />
+      </>
+    )
 }
 
 export default MyApp
