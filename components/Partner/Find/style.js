5f52eb3de2cac8e032da375479a5afe870d230b3
Enma Lidia Muñoz Garcia <enmalidia88@gmail.com>
2022-06-28 13:12:30 -0400

    fix: :recycle: Contact form: adjusting font styling
    #210

--- components/Partner/Find/style.js
+++ components/Partner/Find/style.js
@@ -78,8 +78,8 @@ svg.resizes{
 export const IconContent = styled.div.attrs({
     className: "md:p-2 text-center"
 })``;
-export const IconTitle = styled.h1.attrs({
-    className: "title-font  text-sm sm:text-base md:text-lg font-bold text-primary md:mb-1"
+export const IconTitle = styled.h3.attrs({
+    className: "title-font text-sm sm:text-base md:text-lg font-bold text-primary md:mb-1"
 })``;
 export const IconText = styled.p.attrs({
     className: "text-sm sm:text-base leading-5 text-gray-500 mb-3 mt-0"
